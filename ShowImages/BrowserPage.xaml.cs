8ef81a5dca9e7e6cb4062101bb99be1cd5f7fe8a
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2011-01-19 11:45:06 +0000

    pagine separate

---
+++ ShowImages/BrowserPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using HtmlAgilityPack;
+using Microsoft.Phone.Controls;
+
+namespace ShowImages
+{
+    public partial class BrowserPage : PhoneApplicationPage
+    {
+        public BrowserPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            ChooserWebBrowser.Navigate(Settings.BrowserHistory.Pop());
+        }
+
+        private void PhoneApplicationPage_BackKeyPress(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (Settings.BrowserHistory.IsEmpty) return;
+            e.Cancel = true;
+            Settings.BrowserHistory.Pop(); //Pop da saltare perchè appena navigo lo mette nello stack
+            ChooserWebBrowser.Navigate(Settings.BrowserHistory.Pop());
+        }
+
+        #region UrlTextBox
+
+        private void ImageUrlTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (ImageUrlTextBox.Text.Contains("#http://"))
+                ImageUrlTextBox.Text = ImageUrlTextBox.Text.Remove(0, ImageUrlTextBox.Text.IndexOf("http://"));
+            if (ImageUrlTextBox.Text.StartsWith("http://"))
+                ImageUrlTextBox.Select(7, ImageUrlTextBox.Text.Length - 6);
+        }
+
+        private void ImageUrlTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            Uri u;
+            if (e.Key == Key.Enter && Uri.TryCreate(ImageUrlTextBox.Text, UriKind.Absolute, out u))
+                ChooserWebBrowser.Navigate(new Uri(ImageUrlTextBox.Text));
+        }
+        
+        #endregion
+        
+        #region WebBrowser
+
+        private void ChooserWebBrowser_Navigating(object sender, NavigatingEventArgs e)
+        {
+            LoadingProgress.Visibility = Visibility.Visible;
+        }
+
+        private void ChooserWebBrowser_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            ImageUrlTextBox.Text = ChooserWebBrowser.Source.ToString();
+            Settings.BrowserHistory.Push(ChooserWebBrowser.Source);
+            LoadingProgress.Visibility = Visibility.Collapsed;
+        }
+        
+        #endregion
+
+        #region ApplicationBar Buttons
+
+        private void ShowPicsApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            ExtractImagesHelper.ExtractImages(ChooserWebBrowser.Source, ChooserWebBrowser.SaveToString());
+            NavigationService.Navigate(new Uri("/ImageListPage.xaml", UriKind.Relative));
+        }
+
+        private void GoHomeAppBarButton_Click(object sender, EventArgs e)
+        {
+            ChooserWebBrowser.Navigate(new Uri("http://www.google.com/m/?site=images"));
+        }
+
+        #endregion
+
+    }
+}
---
+++ ShowImages/ExtractImagesHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShowImages
+{
+    public static class ExtractImagesHelper
+    {
+        public static void ExtractImages(Uri pageUrl, string htmlString)
+        {
+            HtmlDocument html = new HtmlDocument();
+            html.LoadHtml(htmlString);
+
+            var ImgLinks = new List<string>();
+            foreach (var img in html.DocumentNode.Descendants("img"))
+            {
+                var src = img.GetAttributeValue("src", string.Empty);
+
+                if (src.Contains("::")) //Google Images
+                {
+                    src = src.Substring(src.IndexOf("::") + 2);
+                    int AndIndex = src.IndexOf("&");
+                    src = "http://" + src.Remove(AndIndex == -1 ? src.Length - 1 : AndIndex);
+                }
+
+                if (src.EndsWith(".jpg", StringComparison.InvariantCultureIgnoreCase))
+                    ImgLinks.Add(FormatImageUrl(pageUrl, src));
+            }
+
+            Settings.CurrentImageList = (from anchor in html.DocumentNode.Descendants("a")
+                                         let href = anchor.GetAttributeValue("href", string.Empty)
+                                         where href.EndsWith(".jpg", StringComparison.InvariantCultureIgnoreCase)
+                                         select FormatImageUrl(pageUrl, href))
+                         .Union(ImgLinks)
+                         .Where(i => !string.IsNullOrEmpty(i))
+                         .ToList();
+        }
+
+        private static string FormatImageUrl(Uri pageUrl, string url)
+        {
+            Uri u;
+            if (Uri.TryCreate(url, UriKind.Absolute, out u))
+                return url;
+
+            var FirstPart = pageUrl.ToString().Remove(pageUrl.ToString().LastIndexOf('/') + 1);
+
+            if (!url.Contains(pageUrl.Host))
+                return FirstPart + url;
+
+            if (!url.StartsWith("http://", StringComparison.InvariantCultureIgnoreCase))
+                return "http://" + url;
+
+            return string.Empty;
+        }
+
+    }
+}

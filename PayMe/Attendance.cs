525d9c7bd8fa62ae67dec5d6344e5503bc9b7e85
Licantrop0_cp <SND\Licantrop0_cp>
2011-01-30 20:43:56 +0000

    currency storing

--- PayMe/Attendance.cs
+++ PayMe/Attendance.cs
@@ -27,9 +27,7 @@ namespace PayMe
         public TimeSpan Duration { get { return EndTime - StartTime - PauseTimeSpan; } }
 
         [DataMember]
-        public double Income { get; set; }
-        [DataMember]
-        public string CurrencySymbol { get; set; }
+        public Money Income { get; set; }
         [DataMember]
         public string CustomerName { get; set; }
         [DataMember]
@@ -40,8 +38,75 @@ namespace PayMe
             StartTime = startTime;
             EndTime = endTime;
             PauseTimeSpan = pauseTimeSpan;
-            Income = income;
-            CurrencySymbol = currencySymbol;
+            Income = new Money(income, currencySymbol);
+        }
+    }
+
+    [DataContract]
+    public class Money
+    {
+        public enum CurrencySymbol
+        {
+            Dollar,
+            Euro,
+            Pound,
+            Yen,
+            Franc
+        }
+
+        [DataMember]
+        public double Value { get; set; }
+
+        [DataMember]
+        public CurrencySymbol Symbol { get; set; }
+
+        public Money(double value, string currency)
+        {
+            Value = value;
+            Symbol = GetCurrencyEnum(currency);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0:0.00} {1}", Value, GetCurrencyText(Symbol));
+        }
+
+        public static string GetCurrencyText(CurrencySymbol cs)
+        {
+            switch (cs)
+            {
+                case CurrencySymbol.Dollar:
+                    return "$";
+                case CurrencySymbol.Euro:
+                    return "€";
+                case CurrencySymbol.Pound:
+                    return "£";
+                case CurrencySymbol.Yen:
+                    return "¥";
+                case CurrencySymbol.Franc:
+                    return "Fr";
+                default:
+                    return "$";
+            }
+        }
+
+        public static CurrencySymbol GetCurrencyEnum(string cs)
+        {
+            switch (cs)
+            {
+                case "$":
+                    return CurrencySymbol.Dollar;
+                case "€":
+                    return CurrencySymbol.Euro;
+                case "£":
+                    return CurrencySymbol.Pound;
+                case "¥":
+                    return CurrencySymbol.Yen;
+                case "Fr":
+                    return CurrencySymbol.Franc;
+                default:
+                    return CurrencySymbol.Dollar;
+            }
         }
     }
 }

cb40ee6c18b13bd5c706b449f065645797e1c6d4
arwen_h_cp <SND\arwen_h_cp>
2011-01-30 20:31:33 +0000

    creazione progetto

---
+++ SgarbiMix/SgarbiMix/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WPCommon;
+
+
+/* 
+    Copyright (c) 2010 WPME
+*/
+
+
+namespace SgarbiMix
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Insulta_Click(object sender, RoutedEventArgs e)
+        {
+            if (TrialManagement.IsTrialMode && TrialManagement.AlreadyOpenedToday)
+            {
+                NavigationService.Navigate(new Uri("/DemoPage.xaml", UriKind.Relative));
+            }
+            else
+            {
+                NavigationService.Navigate(new Uri("/PlaySoundPage.xaml", UriKind.Relative));
+            }
+        }
+
+        private void Disclaimer_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/DisclaimerPage.xaml", UriKind.Relative));
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/AboutPage.xaml", UriKind.Relative));
+        }
+    }
+}

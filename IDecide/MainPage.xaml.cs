b23100a3a81ed5fcd90e1e69015b9abf5ab5f4ad
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2011-02-17 23:02:46 +0000

    implementata rotazione del sacchetto

--- IDecide/MainPage.xaml.cs
+++ IDecide/MainPage.xaml.cs
@@ -15,51 +15,57 @@ using WPCommon;
 
 namespace IDecide
 {
-    public partial class MainPage : PhoneApplicationPage
-    {
-        Random rnd = new Random();
-        public MainPage()
-        {
-            InitializeComponent();
-            ApplicationBar = new ApplicationBar();
-            CreateAppBar();
+	public partial class MainPage : PhoneApplicationPage
+	{
+		Random rnd = new Random();
+		object mutex = new object();
+		public MainPage()
+		{
+			InitializeComponent();
+			ApplicationBar = new ApplicationBar();
+			CreateAppBar();
 
-            var sd = new ShakeDetector(3);
-            sd.ShakeEvent += (sender, e) =>
-            {
-                //Inserire un Mutex
-                Dispatcher.BeginInvoke(() => { DecideButton_Click(sender, null); });
-            };
-            sd.Start();
+			var sd = new ShakeDetector();
+			sd.ShakeDetected += (sender, e) =>
+			{
+				//Inserire un Mutex
+				lock(mutex)
+				{
+					Dispatcher.BeginInvoke(() =>
+					{ DecideButton_Click(sender, null); });
+				}
+			};
+			sd.Start();
 
-        }
+		}
 
-        void sd_ShakeEvent(object sender, EventArgs e)
-        {
-        }
+		private void DecideButton_Click(object sender, RoutedEventArgs e)
+		{
+			RotateButton.Stop();
+			var SelectedChoices = Settings.ChoicesGroup
+				.Where(c => c.Key == Settings.SelectedGroup)
+				.Select(c => c.Value)
+				.ToList();
 
-        private void DecideButton_Click(object sender, RoutedEventArgs e)
-        {
-            var SelectedChoices = Settings.ChoicesGroup
-                .Where(c => c.Key == Settings.SelectedGroup)
-                .Select(c => c.Value)
-                .ToList();
-
-            if (SelectedChoices.Count > 0)
-                MessageBox.Show(SelectedChoices[rnd.Next(SelectedChoices.Count)]);
-            else
-                MessageBox.Show(AppResources.NothingToDecide);
-        }
+			AnswerTextBlock.Text = SelectedChoices.Count > 0 ?
+				SelectedChoices[rnd.Next(SelectedChoices.Count)] :
+				AppResources.NothingToDecide;
+			RotateButton.Begin();
+			//if (SelectedChoices.Count > 0)
+			//    MessageBox.Show(SelectedChoices[rnd.Next(SelectedChoices.Count)]);
+			//else
+			//    MessageBox.Show(AppResources.NothingToDecide);
+		}
 
 
-        private void CreateAppBar()
-        {
-            var EditChoicesAppBarButton = new ApplicationBarIconButton();
-            EditChoicesAppBarButton.IconUri = new Uri("Toolkit.Content\\appbar_settings.png", UriKind.Relative);
-            EditChoicesAppBarButton.Text = AppResources.EditChoices;
-            EditChoicesAppBarButton.Click += delegate(object sender, EventArgs e)
-            { NavigationService.Navigate(new Uri("/GroupChoicesPage.xaml", UriKind.Relative)); };
-            ApplicationBar.Buttons.Add(EditChoicesAppBarButton);
-        }
-    }
+		private void CreateAppBar()
+		{
+			var EditChoicesAppBarButton = new ApplicationBarIconButton();
+			EditChoicesAppBarButton.IconUri = new Uri("Toolkit.Content\\appbar_settings.png", UriKind.Relative);
+			EditChoicesAppBarButton.Text = AppResources.EditChoices;
+			EditChoicesAppBarButton.Click += delegate(object sender, EventArgs e)
+			{ NavigationService.Navigate(new Uri("/GroupChoicesPage.xaml", UriKind.Relative)); };
+			ApplicationBar.Buttons.Add(EditChoicesAppBarButton);
+		}
+	}
 }

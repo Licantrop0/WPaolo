cd8ad691e8c53ab8d47d145b1a3fca41350369b4
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2011-02-20 10:08:35 +0000

    implementato tombstoning e sistemate progressbar

--- ShowImages/Properties/Settings.cs
+++ ShowImages/Properties/Settings.cs
@@ -1,68 +1,47 @@
 ﻿using System;
-using System.IO.IsolatedStorage;
 using System.Collections.Generic;
-using Microsoft.Phone.Shell;
-using System.Linq;
 using System.Collections.ObjectModel;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using Microsoft.Phone.Shell;
 
 namespace ShowImages
 {
     public static class Settings
     {
+        public static readonly Uri HomePage = new Uri("http://www.google.com/m/?site=images");
+
         public static ObservableCollection<SelectionableImage> CurrentImageList
         {
             get
             {
-                if (!IsolatedStorageSettings.ApplicationSettings.Contains("current_image_list") ||
-                    (IsolatedStorageSettings.ApplicationSettings["current_image_list"] as ObservableCollection<SelectionableImage>) == null)
-                    IsolatedStorageSettings.ApplicationSettings["current_image_list"] = new ObservableCollection<SelectionableImage>();
-                return (ObservableCollection<SelectionableImage>)IsolatedStorageSettings.ApplicationSettings["current_image_list"];
+                if (!PhoneApplicationService.Current.State.ContainsKey("current_image_list"))
+                    PhoneApplicationService.Current.State["current_image_list"] = new ObservableCollection<SelectionableImage>();
+                return (ObservableCollection<SelectionableImage>)PhoneApplicationService.Current.State["current_image_list"];
             }
             set
             {
                 if (CurrentImageList != value)
-                    IsolatedStorageSettings.ApplicationSettings["current_image_list"] = value;
+                    PhoneApplicationService.Current.State["current_image_list"] = value;
             }
         }
 
-        public static class BrowserHistory
+        public static Stack<Uri> BrowserHistory
         {
-            public static bool IsEmpty { get { return History.Count <= 1; } }
-
-            private static List<Uri> History
+            get
             {
-                get
-                {
-                    if (!IsolatedStorageSettings.ApplicationSettings.Contains("browser_history"))
-                        IsolatedStorageSettings.ApplicationSettings["browser_history"] =
-                            new List<Uri> { new Uri("http://www.google.com/m/?site=images") };
+                if (!PhoneApplicationService.Current.State.ContainsKey("browser_history"))
+                    PhoneApplicationService.Current.State["browser_history"] = 
+                        new Stack<Uri>(new List<Uri>() { HomePage });
 
-                    return (List<Uri>)IsolatedStorageSettings.ApplicationSettings["browser_history"];
-                }
-                set
-                {
-                    if (History != value)
-                        IsolatedStorageSettings.ApplicationSettings["browser_history"] = value;
-                }
+                return (Stack<Uri>)PhoneApplicationService.Current.State["browser_history"];
             }
-
-            public static void Push(Uri uri)
+            set
             {
-                History.Add(uri);
-
-                //tronco la History al 20° item
-                if (History.Count > 20)
-                    History = History.Skip(10).ToList();
-            }
-
-            public static Uri Pop()
-            {
-                var last = History.Last();
-                History.Remove(last);
-                return last;
+                if (BrowserHistory != value)
+                    PhoneApplicationService.Current.State["browser_history"] = value;
             }
         }
-
     }
 
 }

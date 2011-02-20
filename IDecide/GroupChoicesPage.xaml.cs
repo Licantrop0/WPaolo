5d488a39a177cde2505c222a1fa3c576dc7dd0d4
Licantrop0_cp <SND\Licantrop0_cp>
2011-02-20 16:46:40 +0000

    pagina addedit (ancora con bug)

--- IDecide/GroupChoicesPage.xaml.cs
+++ IDecide/GroupChoicesPage.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using System.Windows.Data;
+using WPCommon;
+using Microsoft.Phone.Shell;
 
 namespace IDecide
 {
@@ -19,36 +21,52 @@ namespace IDecide
         public GroupChoicesPage()
         {
             InitializeComponent();
+            CreateAppBar();
             GroupChoicesListBox.ItemsSource = Settings.ChoicesGroup.Select(c => c.Key).Distinct();
         }
 
         private void RemoveButton_Click(object sender, MouseButtonEventArgs e)
         {
+            var GroupName = ((Rectangle)sender).DataContext as string;
 
+            Settings.ChoicesGroup.Where(i => i.Key == GroupName)
+                .ForEach(i => Settings.ChoicesGroup.Remove(i));
         }
 
         private void EditChoicesButton_Click(object sender, RoutedEventArgs e)
         {
             var GroupName = ((Button)sender).DataContext as string;
             if (GroupName != null)
-                NavigationService.Navigate(new Uri("/EditChoicesPage.xaml?key=" + GroupName, UriKind.Relative));
+                NavigationService.Navigate(new Uri("/AddEditChoicesPage.xaml?key=" + GroupName, UriKind.Relative));
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             Settings.SelectedGroup = ((RadioButton)sender).DataContext as string;
         }
+
+        private void CreateAppBar()
+        {
+            ApplicationBar = new ApplicationBar();
+            var AddChoiceGroupAppBarButton = new ApplicationBarIconButton();
+            AddChoiceGroupAppBarButton.IconUri = new Uri("Toolkit.Content\\add_white.png", UriKind.Relative);
+            AddChoiceGroupAppBarButton.Text = AppResources.AddGroup;
+            AddChoiceGroupAppBarButton.Click += (sender, e) =>
+            {
+                NavigationService.Navigate(new Uri("/AddEditChoicesPage.xaml?key=test", UriKind.Relative));
+            };
+            ApplicationBar.Buttons.Add(AddChoiceGroupAppBarButton);
+        }
+
     }
 
-    public class ResourceTranslator : IValueConverter
+    public class ResourceTranslatorConverter : IValueConverter
     {
-
-        #region IValueConverter Members
-
         //Resource Translator
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return AppResources.ResourceManager.GetString((string)value);
+            var TranslatedString = AppResources.ResourceManager.GetString((string)value);
+            return string.IsNullOrEmpty(TranslatedString) ? value : TranslatedString;
         }
 
         // ConvertBack is not implemented for a OneWay binding.
@@ -56,10 +74,19 @@ namespace IDecide
         {
             throw new NotImplementedException();
         }
-
-        #endregion
     }
 
+    public class AmISelectedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Settings.SelectedGroup == (string)value;
+        }
 
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 
 }

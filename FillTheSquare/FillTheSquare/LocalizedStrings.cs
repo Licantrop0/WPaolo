6d9f7c1233a9c4ff7151863db543ddbb29f08f79
Licantrop0_cp <SND\Licantrop0_cp>
2011-03-16 23:39:26 +0000

    [PhilTheSquare] Implementato Multilanguage

---
+++ FillTheSquare/FillTheSquare/LocalizedStrings.cs
@@ -0,0 +1,10 @@
+﻿using FillTheSquare;
+namespace Localization
+{
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+        public LocalizedStrings() { }
+    }
+}

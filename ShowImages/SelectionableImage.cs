48c390d34db36e30f8abccc26746e9f946221441
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2011-04-06 21:34:47 +0000

    [ShowImages] Removed google image support, set IsChecked = False by default

--- ShowImages/SelectionableImage.cs
+++ ShowImages/SelectionableImage.cs
@@ -25,7 +25,7 @@ namespace ShowImages
         public SelectionableImage(string url)
         {
             Url = url;
-            IsSelected = true;
+            IsSelected = false;
         }
     }
 }

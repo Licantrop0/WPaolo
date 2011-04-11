88342f34c3c57b09d5f62896e9a4b7af4298e71c
Licantrop0_cp <SND\Licantrop0_cp>
2011-04-11 15:36:33 +0000

    [WPCommon] Aggiunto TimeSpan Converter

--- WPCommon/StandardConverters.cs
+++ WPCommon/StandardConverters.cs
@@ -9,21 +9,37 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Data;
+using System.Globalization;
 
 namespace WPCommon
 {
     public class BoolToVisibilityConverter : IValueConverter
     {
-
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            //Attenzione potrebbe anche essere un bool?
             var b = (bool)value;
             return b ? Visibility.Visible : Visibility.Collapsed; 
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
     }
+
+    public class TimeSpanTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var ts = (TimeSpan)value;
+            return string.Format("{0}:{1}:{2}", ts.Hours, ts.Minutes, ts.Seconds);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }

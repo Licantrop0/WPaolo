cf14288373d3e75c0eb43a6e2933ea1e698d1a14
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2011-04-17 14:06:56 +0000

    [ScossaFinta] Aggiungo Achievement Unlocked

---
+++ ScossaFinta/ScossaFinta/AchievementUnlocked.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ScossaFinta
+{
+	public partial class AchievementUnlocked : UserControl
+	{
+        public string AchievementText
+        {
+            get { return AchievementTextBlock.Text; }
+            set { AchievementTextBlock.Text = value; }
+        }
+
+        public void Appear()
+        {
+            VisualStateManager.GoToState(this, "Visible", true);
+        }
+
+		public AchievementUnlocked()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}

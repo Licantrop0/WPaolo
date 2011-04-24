940748d836703105c8ddb72a908750e45809f874
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2011-04-24 11:17:54 +0000

    [SgarbiMix] Versione rilasciata

--- SgarbiMix/PlaySoundPage.xaml.cs
+++ SgarbiMix/PlaySoundPage.xaml.cs
@@ -4,23 +4,21 @@ using System.ComponentModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using Microsoft.Phone.Controls;
-using WPCommon;
-using System.Diagnostics;
 using System.Windows.Threading;
-using Google.AdMob.Ads.WindowsPhone7;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Xna.Framework.Media;
+using WPCommon;
 using Google.AdMob.Ads.WindowsPhone7.WPF;
-
+using Microsoft.Phone.Tasks;
 
 namespace SgarbiMix
 {
     public partial class PlaySoundPage : PhoneApplicationPage
     {
         Random rnd = new Random();
-        private int counter;
 
         //---------------------------------------------------
         private bool _bCanExecuteSound = true;
@@ -28,35 +26,46 @@ namespace SgarbiMix
         private DispatcherTimer tmr = new DispatcherTimer();
         //------------------------------------------------
 
-        private void OnTimerTick(object sender, EventArgs e)
-        {
-            tmr.Stop();
-            _bCanExecuteSound = true;
-        }
-
         public PlaySoundPage()
         {
             InitializeComponent();
+
+            tmr.Interval = TimeSpan.FromMilliseconds(700);
+            tmr.Tick += (sender, e) =>
+            {
+                tmr.Stop();
+                _bCanExecuteSound = true;
+            };
+
             sd.ShakeDetected += (sender, e) =>
             {
                 Dispatcher.BeginInvoke(() => { Sound_Shake(); });
             };
             sd.Start();
+
+            if (WPCommon.TrialManagement.IsTrialMode)
+            {
+                MainGrid.Children.Add(new BannerAd() { AdUnitID = "a14da0253ee7df0" });
+                var BuyFullMenuItem = new ApplicationBarMenuItem("Rimuovi la pubblicità");
+                BuyFullMenuItem.Click += (sender, e) =>
+                {
+                    //Rimando a Sgarbimix senza Ad a pagamento
+                    var detailTask = new MarketplaceDetailTask() { ContentIdentifier = "5925f9d6-483d-e011-854c-00237de2db9e" };
+                    detailTask.Show();
+                };
+                ApplicationBar.MenuItems.Add(BuyFullMenuItem);
+            }
         }
 
         private void Sound_Shake()
         {
-
             if (_bCanExecuteSound)
                 App.Sounds[rnd.Next(App.Sounds.Count)].Play();
 
             // ----------------------------------------------------
             _bCanExecuteSound = false;
-            tmr.Interval = TimeSpan.FromMilliseconds(700);
-            tmr.Tick += OnTimerTick;
             tmr.Start();
             //---------------------------------------------------- 
-
         }
 
         private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
@@ -71,11 +80,11 @@ namespace SgarbiMix
             //Configuro l'evento DoWork
             bw.DoWork += (s, evt) =>
             {
-                //prendo la risorsa dei suoni castandola in un array
+                //prendo la risorsa dei suoni castandola in un array ordinato
                 var sr = SoundsResources.ResourceManager
                     .GetResourceSet(CultureInfo.CurrentCulture, true, true)
                     .Cast<DictionaryEntry>()
-                    .OrderBy(de => de.Key.ToString())
+                    .OrderBy(de => de.Key.ToString().Length)
                     .ToArray();
 
                 for (int i = 0; i < sr.Length; i++)
@@ -92,20 +101,16 @@ namespace SgarbiMix
                 var i = evt.ProgressPercentage;
                 var b = new Button()
                 {
-                    //ATTENZIONE: creo un tag per recuperare l'i-esimo sound
-                    Tag = i,
+                    Tag = i, //ATTENZIONE: creo un tag per recuperare l'i-esimo sound
                     Content = App.Sounds[i].ToString(),
                     Style = (Style)App.Current.Resources["PlayButtonStyle"]
                 };
 
                 b.Click += (button, evt1) =>
                 {
-                    if (CheckTrial())
-                    {
-                        //ATTENZIONE: creo un tag per recuperare l'i-esimo sound
-                        var SoundIndex = (int)((Button)button).Tag;
-                        App.Sounds[SoundIndex].Play();
-                    }
+                    //ATTENZIONE: uso il tag per recuperare l'i-esimo sound
+                    var SoundIndex = (int)((Button)button).Tag;
+                    App.Sounds[SoundIndex].Play();
                 };
 
                 PlayButtonsStackPanel.Children.Add(b);
@@ -114,29 +119,51 @@ namespace SgarbiMix
             bw.RunWorkerAsync();
         }
 
-
-        private bool CheckTrial()
+        private void Base1ApplicationBar_Click(object sender, EventArgs e)
         {
-            //if (TrialManagement.IsTrialMode && (App.alreadyOpen || (counter > (App.Sounds.Count / 4))))
-            //{
-            //    NavigationService.Navigate(new Uri("/DemoPage.xaml", UriKind.Relative));
-            //    return false;
-            //}
-            ////incremento ogni volta che chiamo checkTrial - cioè ad ogni click di suono
-            //counter++;
-            return true;
+            PlayBase("base1");
         }
 
+        private void Base2ApplicationBar_Click(object sender, EventArgs e)
+        {
+            PlayBase("base2");
+        }
 
-        private void DisclaimerApplicationBar_Click(object sender, EventArgs e)
+        private void Base3ApplicationBar_Click(object sender, EventArgs e)
+        {
+            PlayBase("base3");
+        }
+
+        private static void PlayBase(string baseName)
+        {
+            if (AskAndPlayMusic())
+            {
+                if (MediaPlayer.Queue.Count == 1 && MediaPlayer.Queue.ActiveSong.Name == baseName && MediaPlayer.State == MediaState.Playing)
+                    MediaPlayer.Stop();
+                else
+                {
+                    MediaPlayer.Play(Song.FromUri(baseName, new Uri("sounds/" + baseName + ".mp3", UriKind.Relative)));
+                    MediaPlayer.IsRepeating = true;
+                }
+            }
+        }
+
+        public static bool AskAndPlayMusic()
+        {
+            return MediaPlayer.GameHasControl ?
+                true :
+                MessageBox.Show("Vuoi interrompere la canzone corrente e riprodurre la base su cui mixare le frasi di Sgarbi?",
+                    "SgarbiMix", MessageBoxButton.OKCancel) == MessageBoxResult.OK;
+        }
+
+        private void DisclaimerAppBarMenu_Click(object sender, EventArgs e)
         {
             NavigationService.Navigate(new Uri("/DisclaimerPage.xaml", UriKind.Relative));
         }
 
-        private void AboutApplicationBar_Click(object sender, EventArgs e)
+        private void AboutAppBarMenu_Click(object sender, EventArgs e)
         {
             NavigationService.Navigate(new Uri("/AboutPage.xaml", UriKind.Relative));
         }
-
     }
 }

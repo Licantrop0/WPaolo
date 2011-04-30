20b13923c42e96e59f00996f356767771b823272
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2011-04-30 11:41:11 +0000

    [SortTheSquare] rinominata cartella e namespace

---
+++ Sort the Square/Record.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Runtime.Serialization;
+
+namespace SortTheSquare
+{
+    [DataContract]
+    public class Record
+    {
+        public int Id { get { return Date.GetHashCode(); } }
+
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Size { get; set; }
+        [DataMember]
+        public DateTime Date { get; set; }
+        [DataMember]
+        public TimeSpan ElapsedTime { get; set; }
+
+        public Record(int size, DateTime date, TimeSpan elapsedTime)
+        {
+            Size = size + "x" + size;
+            Date = date;
+            ElapsedTime = elapsedTime;
+            Name = string.Empty;
+        }
+    }
+}
---
+++ Sort the Square/RecordsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.Phone.Controls;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace SortTheSquare
+{
+    public partial class RecordsPage : PhoneApplicationPage
+    {
+        public RecordsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //Settings.AddFakeRecords();
+            RecordListBox.ItemsSource = Settings.Records.OrderBy(r => r.ElapsedTime);
+        }
+
+        protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+    }
+}

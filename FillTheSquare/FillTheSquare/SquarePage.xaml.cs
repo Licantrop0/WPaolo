adbcfd9cc3d54cd64fc8bc66a7854f42344902e2
Licantrop0_cp <SND\Licantrop0_cp>
2011-06-14 23:12:06 +0000

    [PhilTheSquare] prima implementazione Mosse Disponibili

--- FillTheSquare/FillTheSquare/SquarePage.xaml.cs
+++ FillTheSquare/FillTheSquare/SquarePage.xaml.cs
@@ -15,117 +15,108 @@ using WPCommon;
 
 namespace FillTheSquare
 {
-	public partial class SquarePage : PhoneApplicationPage
-	{
-		public MagicSquare Square;
-		DispatcherTimer dt;
-		StopwatchWrapper sw;
+    public partial class SquarePage : PhoneApplicationPage
+    {
+        public MagicSquare Square;
+        DispatcherTimer dt;
+        StopwatchWrapper sw;
 
-		public SquarePage()
-		{
-			InitializeComponent();
-			InitializeTimers();
-			InizializeSquare();
-			SettingsViewModel.StopMusicIfControl();
-		}
+        public SquarePage()
+        {
+            InitializeComponent();
+            InitializeTimers();
+            InizializeSquare();
+            SettingsViewModel.StopMusicIfControl();
+        }
 
-		private void InizializeSquare()
-		{
-			Square = new MagicSquare(Settings.CurrentGridSize, Settings.GetGridState());
-			for (int i = 0; i < Settings.CurrentGridSize; i++)
-			{
-				MagicGrid.RowDefinitions.Add(new RowDefinition());
-				MagicGrid.ColumnDefinitions.Add(new ColumnDefinition());
+        private void InizializeSquare()
+        {
+            Square = new MagicSquare(Settings.CurrentGridSize, Settings.GetGridState());
+            for (int i = 0; i < Settings.CurrentGridSize; i++)
+            {
+                MagicGrid.RowDefinitions.Add(new RowDefinition());
+                MagicGrid.ColumnDefinitions.Add(new ColumnDefinition());
 
-				for (int j = 0; j < Settings.CurrentGridSize; j++)
-				{
-					var b = new Border()
-					{
-						Background = (LinearGradientBrush)App.Current.Resources["BorderBackgroundBrush"],
-						BorderThickness = Square.Size == 5 ? new Thickness(2) : new Thickness(1),
-						BorderBrush = new SolidColorBrush(Colors.White),
-					};
+                for (int j = 0; j < Settings.CurrentGridSize; j++)
+                {
+                    var b = new Border()
+                    {
+                        Background = (LinearGradientBrush)App.Current.Resources["BorderBackgroundBrush"],
+                        BorderThickness = Square.Size == 5 ? new Thickness(2) : new Thickness(1),
+                        BorderBrush = new SolidColorBrush(Colors.White),
+                    };
 
-					b.SetRow(i);
-					b.SetColumn(j);
-					b.MouseLeftButtonDown += Button_Click;
-					MagicGrid.Children.Add(b);
-				}
-			}
-		}
+                    b.SetRow(i);
+                    b.SetColumn(j);
+                    b.MouseLeftButtonDown += Button_Click;
+                    MagicGrid.Children.Add(b);
+                }
+            }
+        }
 
-		protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
-		{
-			//ripristino dello stato della griglia
-			var points = Square.PositionHistory.ToArray();
-			Array.Reverse(points);
-			for (int i = 0; i < points.Length; i++)
-			{
-				var b = MagicGrid.Children[Square.Size * points[i].Y + points[i].X] as Border;
-				b.Child = new TextBlock()
-				{
-					Text = (i + 1).ToString(),
-					Style = (Style)Application.Current.Resources["SquareTitleStyle"],
-					FontSize = Square.Size == 5 ? 32 : 28
-				};
-			}
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            //ripristino dello stato della griglia
+            var points = Square.PositionHistory.ToArray();
+            Array.Reverse(points);
+            for (int i = 0; i < points.Length; i++)
+            {
+                var b = MagicGrid.Children[Square.Size * points[i].Y + points[i].X] as Border;
+                b.Child = new TextBlock()
+                {
+                    Text = (i + 1).ToString(),
+                    Style = (Style)Application.Current.Resources["SquareTitleStyle"],
+                    FontSize = Square.Size == 5 ? 32 : 28
+                };
+            }
 
-			//ripristino dello stato
-			if (points.Length > 0)
-				sw.Start();
-		}
+            //ripristino dello stato
+            if (points.Length > 0)
+                sw.Start();
+        }
 
-		protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
-		{
-			//salvo lo stato della griglia e del timer
-			Settings.SetGridState(Square.PositionHistory);
-			Settings.CurrentElapsedTime = sw.Elapsed;
-		}
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            //salvo lo stato della griglia e del timer
+            Settings.SetGridState(Square.PositionHistory);
+            Settings.CurrentElapsedTime = sw.Elapsed;
+        }
 
-		private void InitializeTimers()
-		{
-			dt = new DispatcherTimer();
-			dt.Interval = TimeSpan.FromSeconds(0.1);
-			dt.Tick += (sender, e) =>
-			{
-				TimeElapsedTextBlock.Text = string.Format(
-					AppResources.ElapsedTime, sw.Elapsed.TotalSeconds);
-			};
-			dt.Start();
+        private void InitializeTimers()
+        {
+            dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromSeconds(0.1);
+            dt.Tick += (sender, e) =>
+            {
+                TimeElapsedTextBlock.Text = string.Format(
+                    AppResources.ElapsedTime, sw.Elapsed.TotalSeconds);
+            };
+            dt.Start();
 
-			sw = new StopwatchWrapper(Settings.CurrentElapsedTime);
-		}
+            sw = new StopwatchWrapper(Settings.CurrentElapsedTime);
+        }
 
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-			var currentBorder = (Border)sender;
-			var p = new GridPoint(
-				currentBorder.GetColumn(),
-				currentBorder.GetRow());
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var currentBorder = (Border)sender;
+            var p = new GridPoint(
+                currentBorder.GetColumn(),
+                currentBorder.GetRow());
 
-			//tutta la logica della griglia è dentro il metodo PressButton
-			switch (Square.PressButton(p))
-			{
-				case true: //caso creazione andato a buon fine
-					currentBorder.Child = new TextBlock()
-					{
-						Text = Square.PositionHistory.Count.ToString(),
-						Style = (Style)Application.Current.Resources["SquareTitleStyle"],
-						FontSize = 28
-					};
+            //tutta la logica della griglia è dentro il metodo PressButton
+            switch (Square.PressButton(p))
+            {
+                case true: //caso creazione andato a buon fine
+                    currentBorder.Child = new TextBlock()
+                    {
+                        Text = Square.PositionHistory.Count.ToString(),
+                        Style = (Style)Application.Current.Resources["SquareTitleStyle"],
+                        FontSize = 28
+                    };
 
-					SetFocus.Stop();
-					Storyboard.SetTarget(SetFocus, currentBorder);
-					SetFocus.Begin();
-
-                    //qua devo far venire verdi le caselle disponibili per la mossa successiva
-                    if (Square.AvailablePoints != null)
-                        for (int i = 0; i < Square.AvailablePoints.Count; i++)
-                        {
-                            GreenMeansAvailable.Stop();
-                            Storyboard.SetTarget(GreenMeansAvailable, /* qua ci vanno i border presi dalle posizioni dei GridPoint presenti nello Stack AvailablePoints*/);
-                            GreenMeansAvailable.Begin();
-                        }
+                    SetFocus.Stop();
+                    Storyboard.SetTarget(SetFocus, currentBorder);
+                    SetFocus.Begin();
 
                     if (Square.IsCompleted) //Vittoria!
                     {
@@ -136,73 +127,119 @@ namespace FillTheSquare
                         break;
                     }
 
-					if (Square.GetMovesLeft() == 0)  //non ci sono più mosse disponibili
-					{
-						SoundManager.PlayOhNo();
-						PhilPiangeAppear.Begin();
-						NoMoreMovesTextBlock.Visibility = Visibility.Visible;
-					}
-					else
-					{
-						SoundManager.PlayMove();
-					}
-					break;
+                    var availableMoves = Square.GetAvailableMoves();
 
-				case false: //caso di creazione fallito
-					RedFlash.Stop();
-					Storyboard.SetTarget(RedFlash, currentBorder);
-					RedFlash.Begin();
-					SoundManager.PlayError();
-					break;
+                    if (availableMoves.Count == 0)  //non ci sono più mosse disponibili
+                    {
+                        SoundManager.PlayOhNo();
+                        PhilPiangeAppear.Begin();
+                        NoMoreMovesTextBlock.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        SoundManager.PlayMove();
+                        //qua devo far venire verdi le caselle disponibili per la mossa successiva
 
-				case null: //caso di cancellazione
-					ClearBorder(currentBorder);
-					PhilPiangeDisappear.Begin();
-					NoMoreMovesTextBlock.Visibility = Visibility.Collapsed;
+                        DependencyProperty[] propertyChain = new DependencyProperty[]
+                        {
+                            Border.BackgroundProperty,
+                            GradientBrush.GradientStopsProperty,
+                            GradientStop.ColorProperty
+                        };
 
-					//Evidenzio la casella sull'ultimo premuto se la griglia non è vuota
-					if (!Square.IsEmpty)
-					{
-						var lastValue = Square.PositionHistory.Peek();
-						var lastButton = MagicGrid.Children
-							.Where(b => b.GetRow() == lastValue.Y)
-							.Where(b => b.GetColumn() == lastValue.X).First();
 
-						SetFocus.Stop();
-						Storyboard.SetTarget(SetFocus, lastButton);
-						SetFocus.Begin();
-					}
-					SoundManager.PlayUndo();
-					break;
-			}
+                        GreenMeansAvailable.Stop();
+                        GreenMeansAvailable.Children.Clear();
 
-			if (Square.PositionHistory.Count == 1)
-				sw.Start();
-		}
+                        foreach (var move in availableMoves)
+                        {
+                            var b = MagicGrid.Children[Square.Size * move.Y + move.X] as Border;
 
-		private void ResetButton_Click(object sender, RoutedEventArgs e)
-		{
-			SoundManager.PlayReset();
-			PhilPiangeDisappear.Begin();
-			NoMoreMovesTextBlock.Visibility = Visibility.Collapsed;
+                            var ca1 = new ColorAnimation()
+                            {
+                                Duration = new Duration(TimeSpan.FromSeconds(0.5)),
+                                To = Colors.Green
+                            };
+                            //TODO trovare un altro sistema (questo non è supportato da WP7)
+                            Storyboard.SetTargetProperty(ca1,
+                                new PropertyPath("(0).(1)[0].(2)", propertyChain));
+                            GreenMeansAvailable.Children.Add(ca1);
+                            Storyboard.SetTarget(ca1, b);
+                            
+                            var ca2 = new ColorAnimation()
+                             {
+                                 Duration = new Duration(TimeSpan.FromSeconds(0.5)),
+                                 To = Colors.Green
+                             };
+                            Storyboard.SetTargetProperty(ca2,
+                                new PropertyPath("(0).(1)[1].(2)", propertyChain));
+                            GreenMeansAvailable.Children.Add(ca2);
 
-			sw.Reset();
-			Settings.CurrentElapsedTime = TimeSpan.Zero;
-			Settings.SetGridState(new Stack<GridPoint>());
+                            Storyboard.SetTarget(ca2, b);
 
-			Square.Clear();
+                            // (Border.Background).(GradientBrush.GradientStops)[1].(GradientStop.Color)
+                        }
+                        GreenMeansAvailable.Begin();
+                    }
+                    break;
 
-			MagicGrid.Children
-				.Where(ctrl => ctrl is Border)
-				.Cast<Border>()
-				.ForEach(b => ClearBorder(b));
-		}
+                case false: //caso di creazione fallito
+                    RedFlash.Stop();
+                    Storyboard.SetTarget(RedFlash, currentBorder);
+                    RedFlash.Begin();
+                    SoundManager.PlayError();
+                    break;
 
-		private static void ClearBorder(Border b)
-		{
-			b.Child = null;
-			b.Background = (LinearGradientBrush)App.Current.Resources["BorderBackgroundBrush"];
-		}
+                case null: //caso di cancellazione
+                    ClearBorder(currentBorder);
+                    PhilPiangeDisappear.Begin();
+                    NoMoreMovesTextBlock.Visibility = Visibility.Collapsed;
 
-	}
+                    //Evidenzio la casella sull'ultimo premuto se la griglia non è vuota
+                    if (!Square.IsEmpty)
+                    {
+                        var lastValue = Square.PositionHistory.Peek();
+                        var lastButton = MagicGrid.Children[Square.Size * lastValue.Y + lastValue.X];
+
+                        //MagicGrid.Children
+                        //    .Where(b => b.GetRow() == lastValue.Y)
+                        //    .Where(b => b.GetColumn() == lastValue.X).First();
+
+                        SetFocus.Stop();
+                        Storyboard.SetTarget(SetFocus, lastButton);
+                        SetFocus.Begin();
+                    }
+                    SoundManager.PlayUndo();
+                    break;
+            }
+
+            if (Square.PositionHistory.Count == 1)
+                sw.Start();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            SoundManager.PlayReset();
+            PhilPiangeDisappear.Begin();
+            NoMoreMovesTextBlock.Visibility = Visibility.Collapsed;
+
+            sw.Reset();
+            Settings.CurrentElapsedTime = TimeSpan.Zero;
+            Settings.SetGridState(new Stack<GridPoint>());
+
+            Square.Clear();
+
+            MagicGrid.Children
+                .Where(ctrl => ctrl is Border)
+                .Cast<Border>()
+                .ForEach(b => ClearBorder(b));
+        }
+
+        private static void ClearBorder(Border b)
+        {
+            b.Child = null;
+            b.Background = (LinearGradientBrush)App.Current.Resources["BorderBackgroundBrush"];
+        }
+
+    }
 }

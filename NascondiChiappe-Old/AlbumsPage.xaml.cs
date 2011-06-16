6f9024fcdc7ea9eed5a0c572512735fddeb04a87
Licantrop0_cp <SND\Licantrop0_cp>
2011-06-16 22:28:56 +0000

    [NascondiChiappe] Branch della vecchia versione

---
+++ NascondiChiappe-Old/AlbumsPage.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+using NascondiChiappe.Localization;
+
+namespace NascondiChiappe
+{
+    public partial class AlbumsPage : PhoneApplicationPage
+    {
+        ApplicationBarIconButton CopyToMediaLibraryAppBarButton;
+        List<AlbumPhoto> SelectedPhotos;
+        private Album SelectedAlbum;
+
+        private Album SelectedAlbumWrapper
+        {
+            get
+            {
+                if (!PhoneApplicationService.Current.State.ContainsKey("selected_album"))
+                    PhoneApplicationService.Current.State.Add("selected_album", null);
+
+                var sa = (Album)PhoneApplicationService.Current.State["selected_album"];
+                return sa ?? AppContext.Albums.OrderBy(a => a.Name).FirstOrDefault();
+            }
+            set
+            {
+                if (SelectedAlbumWrapper != value)
+                    PhoneApplicationService.Current.State["selected_album"] = value;
+            }
+        }
+
+        public AlbumsPage()
+        {
+            InitializeComponent();
+            InitializeApplicationBar();
+            csvAlbums.Source = AppContext.Albums;
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (!AppContext.IsPasswordInserted)
+            {
+                NavigationService.Navigate(new Uri("/PasswordPage.xaml", UriKind.Relative));
+                return;
+            }
+            if (AppContext.Albums.Count == 0)
+            {
+                NavigationService.Navigate(new Uri("/AddEditAlbumPage.xaml", UriKind.Relative));
+                return;
+            }
+
+            AlbumsPivot.ItemsSource = AppContext.Albums;
+            SelectedAlbum = SelectedAlbumWrapper;
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            SelectedAlbumWrapper = SelectedAlbum;
+        }
+
+        private void Pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AlbumsPivot.SelectedIndex != -1)
+                SelectedAlbum = e.AddedItems[0] as Album;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedPhotos = ((ListBox)sender).SelectedItems.Cast<AlbumPhoto>().ToList();
+            CopyToMediaLibraryAppBarButton.IsEnabled = SelectedPhotos.Count > 0;
+        }
+
+        private void ImageList_DoubleTap(object sender, GestureEventArgs e)
+        {
+            var photoIndex = SelectedAlbum.Photos.IndexOf((AlbumPhoto)sender);
+            NavigationService.Navigate(new Uri(
+                string.Format("/ViewPhotosPage.xaml?Album={0}&Photo={1}",
+                SelectedAlbum.DirectoryName, photoIndex),
+                UriKind.Relative));
+        }
+
+        void TakePictureAppBarButton_Click(object sender, EventArgs e)
+        {
+            if (IsTrialWithCheck())
+                return;
+
+            var cameraCaptureTask = new CameraCaptureTask();
+            cameraCaptureTask.Completed += CaptureTask_Completed;
+            try
+            {
+                cameraCaptureTask.Show();
+            }
+            catch (InvalidOperationException) { };
+        }
+
+        void CopyFromMediaLibraryAppBarButton_Click(object sender, EventArgs e)
+        {
+            if (IsTrialWithCheck())
+                return;
+
+            var photoChooserTask = new PhotoChooserTask();
+            photoChooserTask.Completed += CaptureTask_Completed;
+            try
+            {
+                photoChooserTask.Show();
+            }
+            catch (InvalidOperationException) { };
+        }
+
+        void CaptureTask_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                var fileName = AlbumPhoto.GetFileNameWithRotation(e.OriginalFileName, e.ChosenPhoto);
+                SelectedAlbum.AddPhoto(new AlbumPhoto(fileName, e.ChosenPhoto));
+                e.ChosenPhoto.Close();
+            }
+        }
+
+        private bool IsTrialWithCheck()
+        {
+            if (WPCommon.TrialManagement.IsTrialMode && SelectedAlbum.Photos.Count >= 4)
+            {
+                NavigationService.Navigate(new Uri("/DemoPage.xaml", UriKind.Relative));
+                return true;
+            }
+            return false;
+        }
+
+        void CopyToMediaLibraryAppBarButton_Click(object sender, EventArgs e)
+        {
+            foreach (var p in SelectedPhotos)
+                SelectedAlbum.CopyToMediaLibrary(p);
+
+            //TODO: tradurre anche francese
+            MessageBox.Show(SelectedPhotos.Count == 1 ?
+                AppResources.PhotoCopied :
+                AppResources.PhotosCopied);
+
+            //TODO: implementare export asincrono con Mango
+            //var bw = new BackgroundWorker();
+            //bw.DoWork += (sender1, e1) =>
+            //{
+            //};
+            //bw.RunWorkerCompleted += (sender1, e1) =>
+            //{
+            //};
+            //bw.RunWorkerAsync();
+        }
+
+        private void InitializeApplicationBar()
+        {
+            //var ViewPhotoAppBarButton = new ApplicationBarIconButton();
+            //ViewPhotoAppBarButton.IconUri = new Uri("Toolkit.Content\\appbar_viewpic.png", UriKind.Relative);
+            //ViewPhotoAppBarButton.Text = AppResources.ViewPhoto;
+            //ViewPhotoAppBarButton.Click += new EventHandler(ViewPhotoAppBarButton_Click);
+            //ApplicationBar.Buttons.Add(ViewPhotoAppBarButton);
+
+            var TakePictureAppBarButton = new ApplicationBarIconButton();
+            TakePictureAppBarButton.IconUri = new Uri("Toolkit.Content\\appbar_camera.png", UriKind.Relative);
+            TakePictureAppBarButton.Text = AppResources.TakePhoto;
+            TakePictureAppBarButton.Click += new EventHandler(TakePictureAppBarButton_Click);
+            ApplicationBar.Buttons.Add(TakePictureAppBarButton);
+
+            var CopyFromMediaLibraryAppBarButton = new ApplicationBarIconButton();
+            CopyFromMediaLibraryAppBarButton.IconUri = new Uri("Toolkit.Content\\appbar_addpicture.png", UriKind.Relative);
+            CopyFromMediaLibraryAppBarButton.Text = AppResources.CopyFromMediaLibrary;
+            CopyFromMediaLibraryAppBarButton.Click += new EventHandler(CopyFromMediaLibraryAppBarButton_Click);
+            ApplicationBar.Buttons.Add(CopyFromMediaLibraryAppBarButton);
+
+            CopyToMediaLibraryAppBarButton = new ApplicationBarIconButton();
+            CopyToMediaLibraryAppBarButton.IconUri = new Uri("Toolkit.Content\\appbar_sendphoto.png", UriKind.Relative);
+            CopyToMediaLibraryAppBarButton.Text = AppResources.CopyToMediaLibrary;
+            CopyToMediaLibraryAppBarButton.IsEnabled = false;
+            CopyToMediaLibraryAppBarButton.Click += new EventHandler(CopyToMediaLibraryAppBarButton_Click);
+            ApplicationBar.Buttons.Add(CopyToMediaLibraryAppBarButton);
+
+            var EditAlbumAppBarMenuItem = new ApplicationBarMenuItem();
+            EditAlbumAppBarMenuItem.Text = AppResources.EditAlbum;
+            EditAlbumAppBarMenuItem.Click += (sender, e) =>
+            { NavigationService.Navigate(new Uri("/AddEditAlbumPage.xaml?Album=" + SelectedAlbum.DirectoryName, UriKind.Relative)); };
+            ApplicationBar.MenuItems.Add(EditAlbumAppBarMenuItem);
+
+            var AddAlbumAppBarMenuItem = new ApplicationBarMenuItem();
+            AddAlbumAppBarMenuItem.Text = AppResources.AddAlbum;
+            AddAlbumAppBarMenuItem.Click += (sender, e) =>
+            { NavigationService.Navigate(new Uri("/AddEditAlbumPage.xaml", UriKind.Relative)); };
+            ApplicationBar.MenuItems.Add(AddAlbumAppBarMenuItem);
+        }
+
+    }
+}
---
+++ NascondiChiappe-Old/PasswordPage.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using NascondiChiappe.Localization;
+
+namespace NascondiChiappe
+{
+    public partial class PasswordPage : PhoneApplicationPage
+    {
+        private bool IsNewPasswordMode { get { return Settings.Password == null; } }
+        private bool IsChangePasswordMode { get { return NavigationContext.QueryString.ContainsKey("ChangePassword"); } }
+
+        public PasswordPage()
+        {
+            InitializeComponent();
+            InitializeApplicationBar();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (IsNewPasswordMode)
+            {
+                TitleTextBlock.Text = AppResources.SetPassword;
+                PasswordStackPanel.Visibility = Visibility.Collapsed;
+                NewPasswordStackPanel.Visibility = Visibility.Visible;
+                NewPasswordBox.Focus();
+            }
+            else if (IsChangePasswordMode)
+            {
+                TitleTextBlock.Text = AppResources.ChangePassword;
+                PasswordStackPanel.Visibility = Visibility.Collapsed;
+                PasswordHintTextBlock.Visibility = Visibility.Collapsed;
+                NewPasswordStackPanel.Visibility = Visibility.Visible;
+                OldPasswordStackPanel.Visibility = Visibility.Visible;
+                OldPasswordBox.Focus();
+            }
+            else //Insert Password Mode
+            {
+                var ChangePasswordAppBarMenuItem = new ApplicationBarMenuItem();
+                ChangePasswordAppBarMenuItem.Text = AppResources.ChangePassword;
+                ChangePasswordAppBarMenuItem.Click += (sender1, e1) =>
+                { NavigationService.Navigate(new Uri("/PasswordPage.xaml?ChangePassword=1", UriKind.Relative)); };
+                ApplicationBar.MenuItems.Add(ChangePasswordAppBarMenuItem);
+
+                MainPasswordBox.Focus();
+            }
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (AppContext.IsPasswordInserted & !IsChangePasswordMode)
+                throw new Exception("ForceExit");
+        }
+
+        private void PhoneApplicationPage_BackKeyPress(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!AppContext.IsPasswordInserted & !IsChangePasswordMode)
+                throw new Exception("ForceExit");
+        }
+
+        private void InitializeApplicationBar()
+        {
+            var OkAppBarButton = new ApplicationBarIconButton();
+            OkAppBarButton.IconUri = new Uri("Toolkit.Content\\appbar_check.png", UriKind.Relative);
+            OkAppBarButton.Text = AppResources.Ok;
+            OkAppBarButton.Click += new EventHandler(OkAppBarButton_Click);
+            ApplicationBar.Buttons.Add(OkAppBarButton);
+        }
+
+        private void ForgotPasswordHLButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(AppResources.Fucked);
+        }
+
+        void OkAppBarButton_Click(object sender, EventArgs e)
+        {
+            if (IsNewPasswordMode || IsChangePasswordMode)
+            {
+                if (IsChangePasswordMode && (Settings.Password != OldPasswordBox.Password)) //Controllo anche la vecchia password
+                {
+                    MessageBox.Show(AppResources.PasswordWrong);
+                    OldPasswordBox.SelectAll();
+                    return;
+                }
+
+                if (ConfirmPasswordBox.Password == string.Empty)
+                {
+                    ConfirmPasswordBox.Focus();
+                    return;
+                }
+
+                if (NewPasswordBox.Password.Length < Settings.PasswordMinLenght)
+                {
+                    MessageBox.Show(string.Format(AppResources.PasswordMinLenght, Settings.PasswordMinLenght));
+                    NewPasswordBox.SelectAll();
+                    return;
+                }
+
+                if (NewPasswordBox.Password != ConfirmPasswordBox.Password)
+                {
+                    MessageBox.Show(AppResources.PasswordsDoesNotMatch);
+                    ConfirmPasswordBox.SelectAll();
+                    return;
+                }
+
+                Settings.Password = NewPasswordBox.Password;
+            }
+            else
+            {
+                if (Settings.Password != MainPasswordBox.Password)
+                {
+                    MessageBox.Show(AppResources.PasswordWrong);
+                    MainPasswordBox.SelectAll();
+                    return;
+                }
+            }
+
+            AppContext.IsPasswordInserted = true;
+            NavigationService.Navigate(new Uri("/AlbumsPage.xaml", UriKind.Relative));
+        }
+
+        private void PasswordBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                OkAppBarButton_Click(sender, EventArgs.Empty);
+        }
+
+        private void OldPasswordBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                NewPasswordBox.Focus();
+        }
+
+        private void NewPasswordBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ConfirmPasswordBox.Focus();
+                ConfirmPasswordBox.SelectAll();
+            }
+        }
+    }
+}

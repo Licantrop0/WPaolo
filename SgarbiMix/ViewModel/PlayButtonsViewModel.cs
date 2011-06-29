788bc3daeaa94c82dcd3b3237e790fd6ad982b34
Licantrop0_cp <SND\Licantrop0_cp>
2011-06-29 16:32:14 +0000

    [SgarbiMix] fix font embedding

--- SgarbiMix/ViewModel/PlayButtonsViewModel.cs
+++ SgarbiMix/ViewModel/PlayButtonsViewModel.cs
@@ -1,22 +1,12 @@
 ﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using System.Collections.Generic;
-using SgarbiMix.Model;
-using System.Collections.ObjectModel;
-using System.Globalization;
 using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.ComponentModel;
+using System.Windows;
 using Microsoft.Xna.Framework.Media;
+using SgarbiMix.Model;
 
 namespace SgarbiMix.ViewModel
 {

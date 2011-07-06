d984999b740d90cccd43c86c01b89cb48ced0b33
arwen_h_cp <SND\arwen_h_cp>
2011-07-06 23:49:39 +0000

    inizio impostazione libreria gamestate manager xna classic

---
+++ BalanceBall/BalanceBall/BalanceBall/InputState.cs
@@ -0,0 +1,28 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace BalanceBall
+{
+
+    // Helper for reading input. This class tracks both
+    // the current and previous state of both input devices, and implements query
+    // methods for high level input actions such as "move up through the menu"
+    // or "pause the game".
+    public class InputState
+    {
+
+        // Constructs a new input state.
+        public InputState()
+        {
+        }
+
+        // Reads the latest state of the keyboard and gamepad.
+        public void Update()
+        {
+
+        }
+
+ 
+    }
+}
+

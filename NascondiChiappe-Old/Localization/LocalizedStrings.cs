b8f54886bf04f14fc5676689efc47d3e31bc7009
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2011-07-11 15:30:24 +0000

    [NascondiChiappe] Eliminazione vecchi progetti

--- NascondiChiappe-Old/Localization/LocalizedStrings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NascondiChiappe.Localization
-{
-    public class LocalizedStrings
-    {
-        private static AppResources _localizedResources = new AppResources();
-        public AppResources LocalizedResources { get { return _localizedResources; } }
-        public LocalizedStrings() { }
-    }
-}

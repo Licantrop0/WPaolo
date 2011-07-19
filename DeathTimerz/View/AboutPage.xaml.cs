0740fbddbcfbe1b82f66b42c9e604be16154d31e
Licantrop0_cp <SND\Licantrop0_cp>
2011-07-19 17:43:50 +0000

    [DeathTimerz] versione 1.5 sul Marketplace

--- DeathTimerz/View/AboutPage.xaml.cs
+++ DeathTimerz/View/AboutPage.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Controls;
-using System.Reflection;
-using DeathTimerz.Localization;
+﻿using Microsoft.Phone.Controls;
 
 namespace DeathTimerz
 {

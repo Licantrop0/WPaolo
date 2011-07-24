9265674a9a237f493df2f43dd5957292b26865dd
WP_Paolo_cp <SND\WP_Paolo_cp>
2011-07-24 22:03:25 +0000

    esempietto di kiwi che combatte stile virus

---
+++ KiwiVirus/KiwiVirus/KiwiVirus/Utilities.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kiwi
+{
+    public static class Utilities
+    {
+        public static double RandomDouble(this Random dice, double min, double max)
+        {
+            return min + dice.NextDouble() * (max - min);
+        }
+    }
+}

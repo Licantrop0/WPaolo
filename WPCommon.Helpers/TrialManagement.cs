afd045e401df29d9410fa5696c0dfa166c6f30a5
Licantrop0_cp <SND\Licantrop0_cp>
2011-07-24 12:49:04 +0000

    [WPCommon] Helper separati

---
+++ WPCommon.Helpers/TrialManagement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO.IsolatedStorage;
+using Microsoft.Phone.Marketplace;
+
+namespace WPCommon.Helpers
+{
+    public static class TrialManagement
+    {
+        private static LicenseInformation li = new LicenseInformation();
+        public static bool IsTrialMode
+        {
+            get
+            {
+#if DEBUG
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+
+        private static DateTime LastOpen
+        {
+            get
+            {
+                if (!IsolatedStorageSettings.ApplicationSettings.Contains("last_open"))
+                    IsolatedStorageSettings.ApplicationSettings["last_open"] = DateTime.Today.AddDays(-1);
+                return (DateTime)IsolatedStorageSettings.ApplicationSettings["last_open"];
+            }
+            set
+            {
+                if (LastOpen != value)
+                    IsolatedStorageSettings.ApplicationSettings["last_open"] = value;
+            }
+        }
+
+        public static bool AlreadyOpenedToday
+        {
+            get
+            {
+                if (LastOpen == DateTime.Today)
+                    return true;
+                else
+                {
+                    LastOpen = DateTime.Today;
+                    return false;
+                }
+            }
+        }
+    }
+}

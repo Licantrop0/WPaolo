70590fbaea9475880b8a17ce44c6e444c5e93c68
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2011-09-08 13:53:46 +0000

    [SheldonMix] MVVMizzato correttamente

--- SheldonMix/SheldonMix/SoundContainer.cs
+++ SheldonMix/SheldonMix/SoundContainer.cs
@@ -1,107 +1,80 @@
 ﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Media;
-using System.IO;
 
-namespace SheldonMix
+namespace SheldonMix.Model
 {
-    // SoundContainer per WAV
-
-    public class SoundContainer
+    public enum SoundType
     {
-
-        string _name;
-        int type;
-        UnmanagedMemoryStream _rawSound;
-
-        private SoundEffect _sound;
-        private SoundEffect Sound
-        {
-            get
-            {
-                if (_sound == null)
-                    _sound = SoundEffect.FromStream(_rawSound);
-                return _sound;
-            }
-        }
-
-        public SoundContainer(string rawName, UnmanagedMemoryStream rawSound)
-        {    
-            //Convenzione per classificare: la prime tre lettere definiscono la classe
-            if (rawName.StartsWith("CLA")) type = 1;
-            else if (rawName.StartsWith("BAZ")) type = 2;
-            else type = 3;
-            rawName.Remove(3);
-            //Convenzione: "_" = spazio, "1" = punto esclamativo  "2" = punto interrogativo
-            _name = rawName.Replace("_", " ").Replace("1", "!").Replace("2","?");
-            _rawSound = rawSound;
-        }
-
-        public bool Play()
-        {
-            return Sound.Play();
-
-            //MediaPlayer.Play(Song.FromUri(baseName, new Uri("sounds/" + baseName + ".mp3", UriKind.Relative)));
-            //MediaPlayer.IsRepeating = true;
-        }
-
-        public override string ToString()
-        {
-            return _name;
-        }
-
-        public int Type()
-        {
-            return type;
-        }
+        CLAS,
+        TBBT,
+        ZAZZ
     }
-
-    // SoundContainer per MP3
 
     public class SoundContainerMP3
     {
-        string _rawName;
-        string _name;
-        int _type;
+        private string _name;
+        public SoundType Category { get; private set; }
+        private string _rawName;
 
         public SoundContainerMP3(string rawName)
         {
-            //Convenzione per classificare: la prime tre lettere definiscono la classe
-            if (rawName.StartsWith("CLAS_")) _type = 1;
-            else if (rawName.StartsWith("ZAZZ_")) _type = 2;
-            else if (rawName.StartsWith("TBBT_")) _type = 3;
-            else
-                _type = 4;
-
-            //Convenzione: "_" = spazio, "1" = punto esclamativo  "2" = punto interrogativo
-            _name = rawName.Substring(5).Replace("_", " ").Replace("1", "!").Replace("2", "?");
             _rawName = rawName;
-            }
+
+            //Convenzione per classificare: la prime 4 lettere definiscono la classe
+            Category = (SoundType)Enum.Parse(typeof(SoundType), rawName.Split('_')[0], false);
+
+            //Convenzioni:
+            //"_" = spazio
+            //"1" = punto esclamativo
+            //"2" = punto interrogativo
+            _name = rawName.Substring(5).Replace("_", " ").Replace("1", "!").Replace("2", "?");
+        }
 
         public void Play()
         {
-            MediaPlayer.Play(Song.FromUri(_rawName, new Uri("sounds/" + _rawName + ".mp3", UriKind.Relative)));
-            MediaPlayer.IsRepeating = false;     
+            var sound = Song.FromUri(_rawName, new Uri("sounds/" + _rawName + ".mp3", UriKind.Relative));            
+            MediaPlayer.Play(sound);
+
+            MediaPlayer.IsRepeating = false;
         }
 
         public override string ToString()
         {
             return _name;
         }
-
-        public int Type()
-        {
-            return _type;
-        }
     }
 
+    //public class SoundContainer
+    //{
+    //    public SoundType Category { get; private set; }
+    //    private SoundEffect _sound;
+    //    private string _name;
+    //    private UnmanagedMemoryStream _rawSound;
+
+    //    public SoundContainer(string rawName, UnmanagedMemoryStream rawSound)
+    //    {
+    //        //Convenzione per classificare: la prime tre lettere definiscono la classe
+    //        Category = (SoundType)Enum.Parse(typeof(SoundType), rawName.Split('_')[0], false);
+
+    //        //Convenzioni:
+    //        //"_" = spazio
+    //        //"1" = punto esclamativo
+    //        //"2" = punto interrogativo
+    //        _name = rawName.Remove(4).Replace("_", " ").Replace("1", "!").Replace("2", "?");
+    //        _rawSound = rawSound;
+    //    }
+
+    //    public bool Play()
+    //    {
+    //        if (_sound == null)
+    //            _sound = SoundEffect.FromStream(_rawSound);
+
+    //        return _sound.Play();
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return _name;
+    //    }
+    //}
 }
---
+++ SheldonMix/SheldonMix/ViewModel/MainViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using Microsoft.Xna.Framework.Media;
+using SheldonMix.Model;
+
+namespace SheldonMix.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private SoundContainerMP3[] _soundResources;
+        public SoundContainerMP3[] SoundResources
+        {
+            get
+            {
+                if (_soundResources == null)
+                    _soundResources = (from de in SoundsResources.ResourceManager
+                                          .GetResourceSet(CultureInfo.CurrentCulture, true, true)
+                                          .Cast<DictionaryEntry>()
+                                       orderby de.Key
+                                       select new SoundContainerMP3(de.Key.ToString())
+                                      ).ToArray();
+                return _soundResources;
+            }
+        }
+
+        private IEnumerable<SoundContainerMP3> _suoniCLAS;
+        public IEnumerable<SoundContainerMP3> SuoniCLAS
+        {
+            get
+            {
+                if (_suoniCLAS == null)
+                    _suoniCLAS = SoundResources.Where(s => s.Category == SoundType.CLAS);
+                return _suoniCLAS;
+            }
+        }
+
+        private IEnumerable<SoundContainerMP3> _suoniTBBT;
+        public IEnumerable<SoundContainerMP3> SuoniTBBT
+        {
+            get
+            {
+                if (_suoniTBBT == null)
+                    _suoniTBBT = SoundResources.Where(s => s.Category == SoundType.TBBT);
+                return _suoniTBBT;
+            }
+        }
+
+        private IEnumerable<SoundContainerMP3> _suoniZAZZ;
+        public IEnumerable<SoundContainerMP3> SuoniZAZZ
+        {
+            get
+            {
+                if (_suoniZAZZ == null)
+                    _suoniZAZZ = SoundResources.Where(s => s.Category == SoundType.ZAZZ);
+                return _suoniZAZZ;
+            }
+        }
+
+
+        public void PlayBase(string baseName)
+        {
+            if (AskAndPlayMusic())
+            {
+                if (MediaPlayer.Queue.Count == 1 && MediaPlayer.Queue.ActiveSong.Name == baseName && MediaPlayer.State == MediaState.Playing)
+                    MediaPlayer.Stop();
+                else
+                {
+                    MediaPlayer.Play(Song.FromUri(baseName, new Uri("sounds/" + baseName + ".mp3", UriKind.Relative)));
+                    MediaPlayer.IsRepeating = true;
+                }
+            }
+        }
+
+        private static bool AskAndPlayMusic()
+        {
+            return MediaPlayer.GameHasControl ?
+                true :
+                MessageBox.Show("Vuoi interrompere la canzone corrente e riprodurre la base su cui mixare le frasi di Sgarbi?",
+                    "SgarbiMix", MessageBoxButton.OKCancel) == MessageBoxResult.OK;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}

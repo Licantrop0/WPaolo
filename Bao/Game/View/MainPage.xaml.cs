649dff767f38e985ca57ddaf8b261e57baaf7519
Licantrop0_cp <SND\Licantrop0_cp>
2011-09-22 16:14:40 +0000

    [Bao] sistemata pagina about e riorganizzata la solution e il project principale

--- Bao/Game/View/MainPage.xaml.cs
+++ Bao/Game/View/MainPage.xaml.cs
@@ -23,27 +23,27 @@ namespace BaoGame
 
         private void newGameButton_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/GamePage.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/View/GamePage.xaml", UriKind.Relative));
         }
 
         private void settingsButton_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/SettingsPage.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/View/SettingsPage.xaml", UriKind.Relative));
         }
 
         private void creditsButton_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/CreditsPage.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/View/CreditsPage.xaml", UriKind.Relative));
         }
 
         private void tutorialButton_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/TutorialPage.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/View/TutorialPage.xaml", UriKind.Relative));
         }
 
         private void extraButton_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/ExtraPage.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/View/ExtraPage.xaml", UriKind.Relative));
         }
 
 

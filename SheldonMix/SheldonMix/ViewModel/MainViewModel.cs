d32caee30b8fdb8e2376d40d151392076f6bd476
Licantrop0_cp <SND\Licantrop0_cp>
2011-09-29 22:32:30 +0000

    [SheldonMix] modificato sistema di caricamento dei file, ridotto della metà il peso dello XAP

--- SheldonMix/SheldonMix/ViewModel/MainViewModel.cs
+++ SheldonMix/SheldonMix/ViewModel/MainViewModel.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Globalization;
 using System.Linq;
 using SheldonMix.Model;
-using System.IO;
-using System.Reflection;
 
 namespace SheldonMix.ViewModel
 {
@@ -19,12 +15,36 @@ namespace SheldonMix.ViewModel
             {
                 if (_soundResources == null)
                 {
-                    var app = Assembly.GetExecutingAssembly();
-                    _soundResources = (from res in app.GetManifestResourceNames()
-                                      where res.StartsWith("SheldonMix.sounds")
-                                      let name = res.Substring(18)
-                                      orderby name
-                                      select new SoundContainerMP3(name)).ToArray();
+                    string[] files =
+                    {
+                        "CLAS_Bazinga1.mp3",
+                        "CLAS_bazinga_one_of_my_jokes.mp3",
+                        "CLAS_change_is_never_fine.mp3",
+                        "CLAS_knock_knock_knock_Penny.mp3",
+                        "CLAS_mua_ah_ah.mp3",
+                        "CLAS_not_bad_it_s_horrible.mp3",
+                        "CLAS_oouh.mp3",
+                        "CLAS_Sheldon_laugh.mp3",
+                        "CLAS_Sheldon_soft_kitty.mp3",
+                        "CLAS_there_there.mp3",
+                        "TBBT_theme_end.mp3",
+                        "TBBT_theme_full.mp3",
+                        "ZAZZ_brave_leonard_song.mp3",
+                        "ZAZZ_Dr_Sheldon_Cooper_FTW.mp3",
+                        "ZAZZ_Engineering_oompa_loompas_of_science.mp3",
+                        "ZAZZ_hello_honeypuffs.mp3",
+                        "ZAZZ_It_s_a_trap1.mp3",
+                        "ZAZZ_i_possess_the_dna_of_leonard_nimoy.mp3",
+                        "ZAZZ_peace_out.mp3",
+                        "ZAZZ_Sheldon_drunk_sings.mp3",
+                        "ZAZZ_To_Sheldon_live_long_and_prosper.mp3",
+                        "ZAZZ_we_re_taking_the_train.mp3"
+                    };
+
+                    _soundResources =
+                        files.OrderBy(f => f)
+                        .Select(f => new SoundContainerMP3(f))
+                        .ToArray();
                 }
                 return _soundResources;
             }

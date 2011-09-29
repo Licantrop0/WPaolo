5b56f272aa539d5471c4a69ddc7825a05e28987c
Licantrop0_cp <SND\Licantrop0_cp>
2011-09-29 13:18:59 +0000

    [EasyCall] primo imbastimento

---
+++ EasyCall/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace EasyCall1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

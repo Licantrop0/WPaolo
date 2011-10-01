b979e9038d29c95c3bacba72d06e356e35f2a7d7
Licantrop0_cp <SND\Licantrop0_cp>
2011-10-01 11:40:37 +0000

    [EasyCall] Aggiunta AboutPage con ADV

--- WPCommon.Controls/About.xaml.cs
+++ WPCommon.Controls/About.xaml.cs
@@ -2,18 +2,39 @@
 using System.Windows;
 using System.Windows.Controls;
 using Microsoft.Phone.Tasks;
+using Microsoft.Advertising.Mobile.UI;
 
 namespace WPCommon.Controls
 {
     public partial class About : UserControl
     {
+        private string _adUnitId = "10022566";
+        /// <summary>Set this value to the Ad Unit ID to serve Ads</summary>
+        public string AdUnitId
+        {
+            get { return _adUnitId; }
+            set { _adUnitId = value; }
+        }
+
         public About()
         {
             InitializeComponent();
         }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var ad1 = new AdControl("21ecdf14-8a3e-49e6-b19a-a94b96b2eb0e", AdUnitId, true)
+            {
+                Height = 80, Width = 480,
+                VerticalAlignment = VerticalAlignment.Bottom
+            };
+
+            MainGrid.Children.Add(ad1);
+        }
+
         private void Facebook_Click(object sender, RoutedEventArgs e)
         {
-            new WebBrowserTask() {  Uri = new Uri("http://m.facebook.com/pages/WP-Mobile-Entertainment/192414040771354") }.Show();
+            new WebBrowserTask() { Uri = new Uri("http://m.facebook.com/pages/WP-Mobile-Entertainment/192414040771354") }.Show();
         }
 
         private void Twitter_Click(object sender, RoutedEventArgs e)
@@ -70,5 +91,6 @@ namespace WPCommon.Controls
             catch (InvalidOperationException)
             { /*do nothing */ }
         }
+
     }
 }

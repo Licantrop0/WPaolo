60a5269e35ea08b66f64ca1f602dfdac444798dc
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2011-10-03 16:52:33 +0000

    [EasyCall] prima implementazione V2

---
+++ EasyCall/CallHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using Microsoft.Phone.Tasks;
+using WPCommon.Helpers;
+
+namespace EasyCall
+{
+    public static class CallHelper
+    {
+        private static PhoneCallTask CallTask = new PhoneCallTask();
+
+        public static void Call(string name, string number)
+        {
+            if (!CheckTrial()) return;
+
+            CallTask.DisplayName = name;
+            CallTask.PhoneNumber = number;
+            CallTask.Show();
+        }
+
+        private static bool CheckTrial()
+        {
+            if (TrialManagement.IsTrialMode)
+            {
+                if (TrialManagement.Counter >= 5)
+                {
+                    MessageBox.Show("I'm sorry, you called too many times for this trial, now it's time to pay!", "Trial Mode", MessageBoxButton.OK);
+                    TrialManagement.Buy();
+                    return false;
+                }
+
+                TrialManagement.IncrementCounter();
+                MessageBox.Show("You have " + (6 - TrialManagement.Counter) + " calls left for this demo", "Trial Mode", MessageBoxButton.OK);
+            }
+            return true;
+        }
+    }
+}
--- EasyCall/ViewModel/ContactViewModel.cs
+++ EasyCall/ViewModel/ContactViewModel.cs
@@ -2,37 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using GalaSoft.MvvmLight;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Media.Imaging;
+using Microsoft.Phone;
 
 namespace EasyCall
 {
-    public class ContactViewModel : ViewModelBase
+    public class ContactViewModel : INotifyPropertyChanged
     {
         public string DisplayName { get; set; }
         public string[] NumberRepresentation { get; set; }
         public string[] Numbers { get; set; }
 
-        public ContactViewModel(string displayName, IEnumerable<string> numbers)
+        private WriteableBitmap _bitmap;
+        public WriteableBitmap Bitmap
+        {
+            get { return _bitmap; }
+            set
+            {
+                _bitmap = value;
+                RaisePropertyChanged("Bitmap");
+            }
+        }
+
+        public ContactViewModel(string displayName, IEnumerable<string> numbers, Stream imageStream)
         {
             DisplayName = displayName;
             NumberRepresentation = TextToNum(displayName);
             Numbers = numbers.Select(n => Regex.Replace(n, @"[\s\-\(\)]", string.Empty)).ToArray();
+            if (imageStream != null)
+            {
+                //da rendere async
+                Bitmap = PictureDecoder.DecodeJpeg(imageStream);
+            }
+        }
+
+        public string SelectedNumber
+        {
+            get { return null; }
+            set
+            {
+                RaisePropertyChanged("SelectedNumber");
+                CallHelper.Call(DisplayName, value);
+            }
         }
 
         private string[] TextToNum(string input)
         {
             if (string.IsNullOrEmpty(input))
                 return new string[0];
-            
+
             input = input.ToLower();
-            var output = input.Split(' ');            
+            var output = input.Split(' ');
 
             for (var w = 0; w < output.Length; w++)
             {
-                var word = output[w].ToCharArray();
-                //Regex.Replace(word, "[abc]", "2");
-                //Regex.Replace(word, "[def]", "3");
+                //Regex.Replace(output[w], "[abc]", "2");
+                //Regex.Replace(output[w], "[def]", "3");
+                //Regex.Replace(output[w], "[ghi]", "4");
+                //Regex.Replace(output[w], "[jkl]", "5");
+                //Regex.Replace(output[w], "[mno]", "6");
+                //Regex.Replace(output[w], "[pqrs]", "7");
+                //Regex.Replace(output[w], "[tuv]", "8");
+                //Regex.Replace(output[w], "[wxyz]", "9");
 
+                var word = output[w].ToCharArray();
                 for (int i = 0; i < word.Length; i++)
                 {
                     if (word[i] == '+') word[i] = '0';
@@ -45,11 +80,17 @@ namespace EasyCall
                     else if (word[i] == 't' || word[i] == 'u' || word[i] == 'v') word[i] = '8';
                     else if (word[i] == 'w' || word[i] == 'x' || word[i] == 'y' || word[i] == 'z') word[i] = '9';
                 }
-
                 output[w] = new string(word);
             }
 
             return output;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
 
     }

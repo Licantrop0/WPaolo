35c01b6cd56de6e8af4cbd4adc50726dbeff9d10
Licantrop0_cp <SND\Licantrop0_cp>
2011-11-02 18:24:14 +0000

    [iCub] completamento imbastitura

--- iCub/ViewModel/MainPanoramaViewModel.cs
+++ iCub/ViewModel/MainPanoramaViewModel.cs
@@ -1,20 +1,34 @@
-﻿using GalaSoft.MvvmLight;
-using iCub.Data;
-using System.Collections.Generic;
-using System;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using GalaSoft.MvvmLight;
+using iCub.Data;
 
 namespace iCub.ViewModel
 {
     public class MainPanoramaViewModel : ViewModelBase
     {
         public IEnumerable<PaperViewModel> Papers { get; private set; }
-        public IEnumerable<ProjectViewModel> Projects { get; private set; }
+        public ObservableCollection<ProjectViewModel> Projects { get; private set; }
 
         public MainPanoramaViewModel()
         {
             Papers = DataService.GetPapers();
-            Projects = DataService.GetProjects();
+            Projects = new ObservableCollection<ProjectViewModel>(DataService.GetProjects());
+        }
+
+        public ProjectViewModel SelectedProject
+        {
+            get { return null; }
+
+            set
+            {
+                foreach (var project in Projects.Except(new[] { value }))
+                {
+                    project.IsExpanded = false;
+                }
+                RaisePropertyChanged("SelectedProject");
+            }
         }
     }
 }

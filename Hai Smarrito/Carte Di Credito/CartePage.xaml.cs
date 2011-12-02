a67db2a412c2413d4e253e34c12b785dfae125df
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2011-12-02 13:03:06 +0000

    [HaiSmarrito] aggiunto descrizione bancomat

---
+++ Hai Smarrito/Carte Di Credito/CartePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+
+namespace HaiSmarrito.Carte_Di_Credito
+{
+    public partial class CartePage : PhoneApplicationPage
+    {
+        public CartePage()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            int id;
+            if (int.TryParse(NavigationContext.QueryString["id"], out id))
+                CartePivot.SelectedIndex = id;
+
+            base.OnNavigatedTo(e);
+        }
+
+
+        private void Execute_Navigation(object sender, NavigationEventArgs e)
+        {
+            NavigationService.Navigate(e.Uri);
+        }
+    }
+}

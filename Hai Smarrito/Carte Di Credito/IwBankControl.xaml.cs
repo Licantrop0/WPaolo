0733cd3b8337d54110a3c33190ad3b8fc51ca328
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2011-12-22 13:01:20 +0000

    [HaiSmarrito] Aggiunte tutte le carte di credito

---
+++ Hai Smarrito/Carte Di Credito/IwBankControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace HaiSmarrito.Carte_Di_Credito
+{
+    public partial class IwBankControl : UserControl
+    {
+        public IwBankControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Italia_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Estero_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

31d4785fb024ccb7461174f7df77ff257f584cc9
Licantrop0_cp <SND\Licantrop0_cp>
2012-01-16 22:10:50 +0000

    [NientePanico] sistemato template bottoni Flags

--- Hai Smarrito/Carte Di Credito/NazioniPage.xaml.cs
+++ Hai Smarrito/Carte Di Credito/NazioniPage.xaml.cs
@@ -1,34 +1,21 @@
-﻿using System.Windows;
+﻿using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
-using System.Reflection;
-using NientePanico.Helpers;
 using NientePanico.ViewModel;
 
 namespace NientePanico.Carte_Di_Credito
 {
     public partial class NazioniPage : PhoneApplicationPage
     {
-        private NazioniViewModel _vM;
-        public NazioniViewModel VM
-        {
-            get
-            {
-                if (_vM == null)
-                    _vM = (NazioniViewModel)LayoutRoot.DataContext;
-
-                return _vM;
-            }
-        }
-
         public NazioniPage()
         {
             InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            VM.CardType = NavigationContext.QueryString["type"];
             base.OnNavigatedTo(e);
+            var VM = (NazioniViewModel)LayoutRoot.DataContext;
+            VM.CardType = NavigationContext.QueryString["type"];
         }
     }
 }

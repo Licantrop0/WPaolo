b454325fec2c059bae6641e72b188e1ff5d84055
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2012-01-19 21:49:30 +0000

    [niente panico] bancoposta finito

--- Hai Smarrito/Carte Di Credito/BancopostaControl.xaml.cs
+++ Hai Smarrito/Carte Di Credito/BancopostaControl.xaml.cs
@@ -13,32 +13,32 @@ namespace NientePanico.Carte_Di_Credito
 
         private void PostePayItalia_Click(object sender, RoutedEventArgs e)
         {
-            CallHelper.Call("Bancoposta Italia", "800822056");
+            CallHelper.Call("PostePayItalia", "800902122");
         }
 
         private void PostePayEstero_Click(object sender, RoutedEventArgs e)
         {
-
+            CallHelper.Call("PostePayEstero", "00390234980131");
         }
 
         private void PostamatItalia_Click(object sender, RoutedEventArgs e)
         {
-
+            CallHelper.Call("PostamatItalia", "800652653");
         }
 
         private void PostamatEstero_Click(object sender, RoutedEventArgs e)
         {
-
+            CallHelper.Call("PostamatItalia", "00390234980132");
         }
 
         private void BancoPostaItalia_Click(object sender, RoutedEventArgs e)
         {
-
+            CallHelper.Call("BancoPostaItalia", "800207167");
         }
 
         private void BancoPostaEstero_Click(object sender, RoutedEventArgs e)
         {
-
+            CallHelper.Call("BancoPostaEstero", "00390432744106");
         }
 
         private void Italia_Click(object sender, RoutedEventArgs e)

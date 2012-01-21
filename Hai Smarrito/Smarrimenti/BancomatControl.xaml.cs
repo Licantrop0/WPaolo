aa234cd4d97ebd78ceeb3e51222cbbdcb7620d54
Licantrop0_cp <SND\Licantrop0_cp>
2012-01-21 22:14:46 +0000

    [niente panico] bancomat, telepass

--- Hai Smarrito/Smarrimenti/BancomatControl.xaml.cs
+++ Hai Smarrito/Smarrimenti/BancomatControl.xaml.cs
@@ -13,12 +13,12 @@ namespace NientePanico.Smarrimenti
 
         private void Italia_Click(object sender, RoutedEventArgs e)
         {
-            CallHelper.Call("Bancomat Italia", "800 383838383");
+            CallHelper.Call("Bancomat Italia", "800822056");
         }
 
         private void Estero_Click(object sender, RoutedEventArgs e)
         {
-            CallHelper.Call("Bancomat Estero", "800 383838383");
+            CallHelper.Call("Bancomat Estero", "00390245403768");
         }
     }
 }

35edd86103fc13e66dae28054cde6bba77950ddf
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2012-01-27 16:14:47 +0000

    [NientePanico] aggiunta pagina di inserimento carta o documento

---
+++ Hai Smarrito/CardDataPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Phone.Controls;
+using NientePanico.ViewModel;
+
+namespace NientePanico
+{
+    /// <summary>
+    /// Description for CardDataPage.
+    /// </summary>
+    public partial class CardDataPage : PhoneApplicationPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the CardDataPage class.
+        /// </summary>
+        public CardDataPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_BackKeyPress(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(NameTextBox.Text))
+                return;
+
+            var vm = (CardDataViewModel)LayoutRoot.DataContext;
+
+            if (!vm.IsEditMode)
+                AppContext.Cards.Add(vm);
+        }
+    }
+}

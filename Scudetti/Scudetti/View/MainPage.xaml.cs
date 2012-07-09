7a6d0c5945faa1daabe3f2ce721aed400a6a0625
Licantrop0_cp <SND\Licantrop0_cp>
2012-07-09 17:17:25 +0000

    [Scudetti] aggiunto AdControl

--- Scudetti/Scudetti/View/MainPage.xaml.cs
+++ Scudetti/Scudetti/View/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows.Media.Animation;
 using Microsoft.Phone.Controls;
 using System.Windows.Media;
+using Microsoft.Advertising.Mobile.UI;
 
 namespace Scudetti.View
 {
@@ -11,5 +12,25 @@ namespace Scudetti.View
         {
             InitializeComponent();
         }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (AdPlaceHolder.Children.Count == 1) //l'Ad c'è già
+                return;
+
+            AdPlaceHolder.Children.Add(new AdControl("489510bc-7ee7-4d2d-9bf1-9065ff63354d", "10040107", true)
+            {
+                Height = 80,
+                Width = 480,
+            });
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            AdPlaceHolder.Children.Clear();
+            base.OnNavigatedFrom(e);
+        }
+
     }
 }

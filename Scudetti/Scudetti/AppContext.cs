58326225cd2182f0cc4a38567cf96cfc8facbcca
Licantrop0_cp <SND\Licantrop0_cp>
2012-07-09 13:03:28 +0000

    [Scudetti] funge?

--- Scudetti/Scudetti/AppContext.cs
+++ Scudetti/Scudetti/AppContext.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
 using Scudetti.Model;
 using System.Linq;
+using Scudetti.ViewModel;
 
 namespace Scudetti
 {
     public static class AppContext
     {
+        public static int TotalShieldUnlocked
+        { get { return Shields.Count(s => s.IsValidated); } }
+
         private static IEnumerable<Shield> _shields;
         public static IEnumerable<Shield> Shields
         {
@@ -17,8 +21,19 @@ namespace Scudetti
             }
         }
 
-        public static int TotalShieldUnlocked
-        { get { return Shields.Count(s => s.IsUnlocked); } }
-
+        private static List<LevelViewModel> _levels;
+        public static List<LevelViewModel> Levels
+        {
+            get
+            {
+                if (_levels == null)
+                {
+                    _levels = AppContext.Shields
+                        .GroupBy(s => s.Level)
+                        .Select(g => new LevelViewModel(g)).ToList();
+                }
+                return _levels;
+            }
+        }
     }
 }
--- Scudetti/Scudetti/ViewModel/LevelViewModel.cs
+++ Scudetti/Scudetti/ViewModel/LevelViewModel.cs
@@ -3,40 +3,58 @@ using GalaSoft.MvvmLight;
 using Scudetti.Model;
 using System.Collections.Generic;
 using Scudetti.Localization;
+using Scudetti.Data;
+using System;
 
 namespace Scudetti.ViewModel
 {
     public class LevelViewModel : ViewModelBase
     {
         public int Number { get; private set; }
+        public string LevelName { get { return string.Format("{0} {1}", AppResources.Level, Number); } }
         public IEnumerable<Shield> Shields { get; private set; }
+        public int TotalShields { get { return Shields.Count(); } }
+        public int CompletedShields { get { return Shields.Count(s => s.IsValidated); } }
+
+        private int LockTreshold = 3;
 
         public bool IsUnlocked
         {
             get
             {
-                if (Number == 1)
-                {
-                    return true;
-                }
-                else
-                {
-                    return AppContext.TotalShieldUnlocked == (Number - 1) * 15;
-                }
+                return  Number == 1 ? true :
+                    AppContext.TotalShieldUnlocked >= (Number - 1) * LockTreshold;
             }
         }
-
-        public int TotalShields { get { return Shields.Count(); } }
-        public int CompletedShields { get { return Shields.Count(s => s.IsUnlocked); } }
-        public string LevelName { get { return string.Format("{0} {1}", AppResources.Level, Number); } }
 
         public string StatusText
         {
             get
             {
                 return IsUnlocked ?
-                    string.Format("{0}/{1}", CompletedShields, TotalShields) :
-                    string.Format(AppResources.Locked, 15 * (Number - 1) - AppContext.TotalShieldUnlocked);
+                    string.Format("{0} {1}/{2}", AppResources.Shields, CompletedShields, TotalShields) :
+                    string.Format(AppResources.Locked, LockTreshold * (Number - 1) - AppContext.TotalShieldUnlocked);
+            }
+        }
+
+        public Shield SelectedShield
+        {
+            get { return null; }
+            set
+            {
+                if (value.IsValidated) return;
+                MessengerInstance.Send<Uri>(new Uri("/View/ShieldPage.xaml?id="
+                    + value.Id, UriKind.Relative), "navigation");
+                RaisePropertyChanged("SelectedShield");
+            }
+        }
+
+        public LevelViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                Number = 1;
+                Shields = DesignTimeData.Shields.Where(s => s.Level == Number);
             }
         }
 
@@ -45,17 +63,23 @@ namespace Scudetti.ViewModel
             Number = group.Key;
             Shields = group;
 
-            foreach (var shield in Shields)
+            foreach (var shield in AppContext.Shields)
             {
                 shield.PropertyChanged += (sender, e) =>
                 {
-                    if (e.PropertyName == "IsUnlocked")
+                    if (e.PropertyName == "IsValidated")
                     {
-                        RaisePropertyChanged("CompletedShields");
-                        RaisePropertyChanged("IsUnlocked");
+                        UpdateLockStatus();
                     }
                 };
             }
+        }
+
+        public void UpdateLockStatus()
+        {
+            RaisePropertyChanged("CompletedShields");
+            RaisePropertyChanged("IsUnlocked");
+            RaisePropertyChanged("StatusText");
         }
     }
 }

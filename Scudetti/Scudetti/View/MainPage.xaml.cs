5a1a43a7405e83b3bed478c3a408036c9118d1c3
Licantrop0_cp <SND\Licantrop0_cp>
2012-08-04 14:07:05 +0000

    [Scudetti] Aggiunto SplashScreen bug: non viene visualizzata correttamente l'immagine del PlayButton se si indovinano tutti gli scudetti

--- Scudetti/Scudetti/View/MainPage.xaml.cs
+++ Scudetti/Scudetti/View/MainPage.xaml.cs
@@ -7,54 +7,53 @@ using System.Windows.Controls;
 
 namespace Scudetti.View
 {
-    public partial class MainPage : PhoneApplicationPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+	public partial class MainPage : PhoneApplicationPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
 
-        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
-        {
-            if (AppContext.Shields != null && AppContext.Shields.All(s => s.IsValidated))
-            {
-                PlayButton.Template = (ControlTemplate)this.Resources["WinButtonTemplate"];
-            }
+		protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+		{
+			//se tutti gli scudetti sono completati, mostra lo scudetto al posto del pallone
+			if (AppContext.Shields != null && AppContext.Shields.All(s => s.IsValidated))
+				PlayButton.Template = (ControlTemplate)this.Resources["WinButtonTemplate"];
 
-            if (AdPlaceHolder.Children.Count == 1) //l'Ad c'è già
-                return;
+			if (AdPlaceHolder.Children.Count == 1) //l'Ad c'è già
+				return;
 
-            AdPlaceHolder.Children.Add(new AdControl("489510bc-7ee7-4d2d-9bf1-9065ff63354d", "10040107", true)
-            {
-                Height = 80,
-                Width = 480,
-            });
+			AdPlaceHolder.Children.Add(new AdControl("489510bc-7ee7-4d2d-9bf1-9065ff63354d", "10040107", true)
+			{
+				Height = 80,
+				Width = 480,
+			});
 
-            base.OnNavigatedTo(e);
-        }
+			base.OnNavigatedTo(e);
+		}
 
-        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
-        {
-            AdPlaceHolder.Children.Clear();
-            base.OnNavigatedFrom(e);
-        }
+		protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+		{
+			AdPlaceHolder.Children.Clear();
+			base.OnNavigatedFrom(e);
+		}
 
-        private void PlayButton_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            SoundManager.PlayKick();
-            NavigationService.Navigate(new Uri("/View/LevelsPage.xaml", UriKind.Relative));
-        }
+		private void PlayButton_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			SoundManager.PlayKick();
+			NavigationService.Navigate(new Uri("/View/LevelsPage.xaml", UriKind.Relative));
+		}
 
-        private void SettingsButton_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            SoundManager.PlayFischietto();
-            NavigationService.Navigate(new Uri("/View/SettingsPage.xaml", UriKind.Relative));
-        }
+		private void SettingsButton_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			SoundManager.PlayFischietto();
+			NavigationService.Navigate(new Uri("/View/SettingsPage.xaml", UriKind.Relative));
+		}
 
-        private void RulesButton_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            SoundManager.PlayFischietto();
-            NavigationService.Navigate(new Uri("/View/RulesPage.xaml", UriKind.Relative));
-        }
-    }
+		private void RulesButton_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			SoundManager.PlayFischietto();
+			NavigationService.Navigate(new Uri("/View/RulesPage.xaml", UriKind.Relative));
+		}
+	}
 }
--- Scudetti/Scudetti/ViewModel/ShieldViewModel.cs
+++ Scudetti/Scudetti/ViewModel/ShieldViewModel.cs
@@ -9,96 +9,101 @@ using Scudetti.Model;
 
 namespace Scudetti.ViewModel
 {
-    public class ShieldViewModel : ViewModelBase
-    {
-        public string InputShieldName { get; set; }
-        public Visibility InputVisibile { get { return CurrentShield.IsValidated ? Visibility.Collapsed : Visibility.Visible; } }
-        public string OriginalName { get { return CurrentShield.Names[0]; } }
+	public class ShieldViewModel : ViewModelBase
+	{
+		public string InputShieldName { get; set; }
+		public Visibility InputVisibile { get { return CurrentShield.IsValidated ? Visibility.Collapsed : Visibility.Visible; } }
+		public string OriginalName { get { return CurrentShield.Names[0]; } }
 
-        private string _hintText = string.Format(AppResources.AvailableHints, AppContext.AvailableHints);
-        public string HintText
-        {
-            get { return _hintText; }
-            set
-            {
-                _hintText = value;
-                RaisePropertyChanged("HintText");
-            }
-        }
+		private string _hintText;
+		public string HintText
+		{
+			get
+			{
+				return _hintText ?? (_hintText = string.Format(AppResources.AvailableHints, AppContext.AvailableHints));
+			}
+			set
+			{
+				_hintText = value;
+				RaisePropertyChanged("HintText");
+			}
+		}
 
-        private Shield _currentShield;
-        public Shield CurrentShield
-        {
-            get { return _currentShield; }
-            set
-            {
-                if (CurrentShield == value)
-                    return;
-                _currentShield = value;
+		private Shield _currentShield;
+		public Shield CurrentShield
+		{
+			get { return _currentShield; }
+			set
+			{
+				if (CurrentShield == value)
+					return;
+				_currentShield = value;
 
-                _currentShield.PropertyChanged += (sender, e) =>
-                {
-                    if (e.PropertyName == "IsValidated")
-                    {
-                        MessengerInstance.Send(new PropertyChangedMessage<bool>(
-                            !_currentShield.IsValidated,
-                            _currentShield.IsValidated,
-                            e.PropertyName));
-                    }
-                };
-                RaisePropertyChanged("CurrentShield");
-            }
-        }
+				_currentShield.PropertyChanged += (sender, e) =>
+				{
+					if (e.PropertyName == "IsValidated")
+					{
+						MessengerInstance.Send(new PropertyChangedMessage<bool>(
+							!_currentShield.IsValidated,
+							_currentShield.IsValidated,
+							e.PropertyName));
+					}
+				};
+				RaisePropertyChanged("CurrentShield");
+			}
+		}
 
-        public ShieldViewModel()
-        {
-            if (IsInDesignMode)
-            {
-                CurrentShield = DesignTimeData.Shields.First();
-            }
-            InputShieldName = string.Empty;
-        }
+		public ShieldViewModel()
+		{
+			if (IsInDesignMode)
+			{
+				_currentShield = DesignTimeData.Shields.First();
+				_currentShield.IsValidated = false;
+				_hintText = string.Format(AppResources.AvailableHints, 5);
+			}
+			InputShieldName = string.Empty;
+		}
 
-        public bool Validate()
-        {
-            if (CurrentShield.IsValidated || string.IsNullOrEmpty(InputShieldName))
-            {
-                MessengerInstance.Send("goback", "navigation");
-            }
-            else if (CurrentShield.Names.Any(name => CompareName(name, InputShieldName)))
-            {
-                CurrentShield.IsValidated = true;
-                if (AppContext.TotalShieldUnlocked % 5 == 0)
-                    AppContext.AvailableHints++;
+		public bool Validate()
+		{
+			if (CurrentShield.IsValidated || string.IsNullOrEmpty(InputShieldName))
+			{
+				MessengerInstance.Send("goback", "navigation");
+			}
+			else if (CurrentShield.Names.Any(name => CompareName(name, InputShieldName)))
+			{
+				CurrentShield.IsValidated = true;
+				if (AppContext.TotalShieldUnlocked % 5 == 0)
+					AppContext.AvailableHints++;
 
-                MessengerInstance.Send("goback", "navigation");
-            }
-            else
-            {
-                MessageBox.Show(AppResources.Wrong);
-                return false;
-            }
+				MessengerInstance.Send("goback", "navigation");
+			}
+			else
+			{
+				MessageBox.Show(AppResources.Wrong);
+				return false;
+			}
 
-            return true;
-        }
+			return true;
+		}
 
-        private bool CompareName(string string1, string string2)
-        {
-            return string.Compare(string1, string2.Trim(), StringComparison.InvariantCultureIgnoreCase) == 0;
-        }
+		private bool CompareName(string string1, string string2)
+		{
+			return string.Compare(string1, string2.Trim(), StringComparison.InvariantCultureIgnoreCase) == 0;
+		}
 
-        public void ShowHint()
-        {
-            if (AppContext.AvailableHints > 0)
-            {
-                HintText = CurrentShield.Hint;
-                AppContext.AvailableHints--;
-            }
-            else
-            {
-                MessageBox.Show(AppResources.NoHintsAvailable);
-            }
-        }
-    }
+		public void ShowHint()
+		{
+			if (AppContext.AvailableHints > 0)
+			{
+				HintText = CurrentShield.Hint;
+				AppContext.AvailableHints--;
+			}
+			else
+			{
+				MessageBox.Show(AppResources.NoHintsAvailable);
+			}
+		}
+	}
 }
 

601827cea6173ae9bc4372a16ae376bc6b55e4c2
Licantrop0_cp <SND\Licantrop0_cp>
2012-08-24 22:48:18 +0000

    Aggiunto Project Win8

---
+++ Scudetti/SocceramaWin8/Model/Shield.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace Scudetti.Model
+{
+    public class Shield : INotifyPropertyChanged
+    {
+        [XmlAttribute]
+        public string Id { get; set; }
+
+        [XmlElement]
+        public string[] Names { get; set; }
+
+        [XmlAttribute]
+        public string Image { get; set; }
+
+        [XmlAttribute]
+        public string Hint { get; set; }
+
+        [XmlAttribute]
+        public int Level { get; set; }
+
+        private bool _isValidated = false;
+        [XmlAttribute]
+        public bool IsValidated
+        {
+            get { return _isValidated; }
+            set
+            {
+                if (IsValidated == value) return;
+                _isValidated = value;
+                RaisePropertyChanged("IsValidated");
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, Lv: {1}", Names[0], Level);
+        }
+
+        #region Interface Implementations
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ Scudetti/SocceramaWin8/Model/ShieldService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Serialization;
+using Windows.ApplicationModel;
+using Windows.Storage;
+
+namespace Scudetti.Model
+{
+    public static class ShieldService
+    {
+        static ApplicationData appData = Windows.Storage.ApplicationData.Current;
+        static XmlSerializer serializer = new XmlSerializer(typeof(Shield[]));
+
+        public static async void Save(IEnumerable<Shield> scudetti)
+        {
+            var file = await appData.RoamingFolder.CreateFileAsync("Shields.xml", CreationCollisionOption.ReplaceExisting);
+
+            using (var stream = await file.OpenStreamForWriteAsync())
+            {
+                serializer.Serialize(stream, scudetti);
+                stream.Flush();
+            }
+        }
+
+        public static async Task<IEnumerable<Shield>> Load()
+        {
+#if !DEBUG
+            try
+            {
+                var file = await appData.RoamingFolder.GetFileAsync("Shields.xml");
+
+                using (var stream = await file.OpenStreamForReadAsync())
+                {
+                    var obj = (Shield[])serializer.Deserialize(stream);
+                    stream.Flush();
+                    return obj;
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                return GetNew();
+            }
+            catch (IOException)
+            {
+                //Unable to load contents of file
+                throw;
+            }
+#else
+            return await GetNew();
+#endif
+
+        }
+
+        public static async Task<IEnumerable<Shield>> GetNew()
+        {
+            string myFile = Path.Combine(Package.Current.InstalledLocation.Path, "Data\\Shields.xml");
+            StorageFolder myFolder = await StorageFolder.GetFolderFromPathAsync(Path.GetDirectoryName(myFile));
+
+            using (var stream = await myFolder.OpenStreamForReadAsync(Path.GetFileName(myFile)))
+            {
+                return (Shield[])serializer.Deserialize(stream);
+
+                //var shields = _xml.Deserialize(stream) as Shield[];
+                //shields.Shuffle();
+                //return shields;
+            }
+        }
+    }
+}

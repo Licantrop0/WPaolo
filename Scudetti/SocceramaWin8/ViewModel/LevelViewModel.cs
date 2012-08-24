eb296eb839657cc8ac4907dfe2ba7a9c7ca07f6c
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2012-08-24 22:48:18 +0000

    Aggiunto Project Win8

---
+++ Scudetti/SocceramaWin8/ViewModel/LevelViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using Scudetti.Model;
+
+namespace SocceramaWin8.ViewModel
+{
+    public class LevelViewModel : ViewModelBase
+    {
+        private IEnumerable<Shield> _shields;
+        public IEnumerable<Shield> Shields
+        {
+            get { return _shields; }
+            set
+            {
+                _shields = value;
+                RaisePropertyChanged("Shields");
+            }
+        }
+
+        public LevelViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                LoadData();
+            }
+        }
+
+        async void LoadData()
+        {
+            Shields = await ShieldService.Load();
+        }
+    }
+}

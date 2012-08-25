b0b4725ceb5b3159e4116c49642d93db00c44db3
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2012-08-25 12:35:53 +0000

    evviva

---
+++ Scudetti/SocceramaWin8/Helper/ImagePathConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace SocceramaWin8.Helper
+{
+    class ImagePathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var path = string.Format("ms-appx:/Assets/Scudetti/{0}", value);
+            return new Uri(path);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

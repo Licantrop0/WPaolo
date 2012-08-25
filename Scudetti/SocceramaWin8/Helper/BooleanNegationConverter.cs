34f6fc50a036b0d52d0e5f0640a49837875fd3f7
Licantrop0_cp <SND\Licantrop0_cp>
2012-08-25 12:35:53 +0000

    evviva

--- Scudetti/SocceramaWin8/Helper/BooleanNegationConverter.cs
+++ Scudetti/SocceramaWin8/Helper/BooleanNegationConverter.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Windows.UI.Xaml.Data;
 
-namespace SocceramaWin8.Common
+namespace SocceramaWin8.Helper
 {
     /// <summary>
     /// Value converter that translates true to false and vice versa.
--- Scudetti/SocceramaWin8/ViewModel/LevelViewModel.cs
+++ Scudetti/SocceramaWin8/ViewModel/LevelViewModel.cs
@@ -5,21 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
 using Scudetti.Model;
+using Windows.UI.Xaml.Data;
 
 namespace SocceramaWin8.ViewModel
 {
     public class LevelViewModel : ViewModelBase
     {
-        private IEnumerable<Shield> _shields;
-        public IEnumerable<Shield> Shields
-        {
-            get { return _shields; }
-            set
-            {
-                _shields = value;
-                RaisePropertyChanged("Shields");
-            }
-        }
+
+        //private IEnumerable<IGrouping<int, Shield>> _levels;
+        //public IEnumerable<IGrouping<int, Shield>> Levels
+        //{
+        //    get { return _levels; }
+        //    set
+        //    {
+        //        _levels = value;
+        //        RaisePropertyChanged("Levels");
+        //    }
+        //}
+
+        //private IEnumerable<Shield> _shields;
+        //public IEnumerable<Shield> Shields
+        //{
+        //    get { return _shields; }
+        //    set
+        //    {
+        //        _shields = value;
+        //        RaisePropertyChanged("Shields");
+        //    }
+        //}
+
+        public CollectionViewSource cvs { get; set; }
 
         public LevelViewModel()
         {
@@ -31,7 +46,13 @@ namespace SocceramaWin8.ViewModel
 
         async void LoadData()
         {
-            Shields = await ShieldService.Load();
+            var shields = await ShieldService.Load();
+            cvs = new CollectionViewSource()
+            {
+                IsSourceGrouped = true,
+                Source =  shields.GroupBy(s => s.Level)
+            };
+            RaisePropertyChanged("cvs");
         }
     }
 }

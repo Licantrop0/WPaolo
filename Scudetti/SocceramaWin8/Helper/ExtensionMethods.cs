3c126a7d66983cd4fbbb720f016a0360e24342e1
Licantrop0_cp <SND\Licantrop0_cp>
2012-10-13 22:54:54 +0000

    aggiunto livello 7, aggiunta procedura upgrade su win8

---
+++ Scudetti/SocceramaWin8/Helper/ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocceramaWin8.Helper
+{
+    public static class ExtensionMethods
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            var rnd = new Random();
+
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}

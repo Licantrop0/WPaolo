7337cf2668c1515b4fded0592696487f65bb20fc
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2013-01-09 22:43:48 +0000

    live tile fixed

---
+++ DeathTimerz/HealthAdvices/HealthAdvice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+
+namespace HealthAdvices
+{
+    public static class HealthAdvice
+    {
+        private static string[] _advices;
+        private static string[] Advices
+        {
+            get
+            {
+                if (_advices == null)
+                    _advices = HealthAdvices.ResourceManager
+                        .GetResourceSet(CultureInfo.CurrentCulture, true, true)
+                        .Cast<DictionaryEntry>()
+                        .Select(item => item.Value.ToString())
+                        .ToArray();
+
+                return _advices;
+            }
+        }
+
+        public static string GetAdviceOfTheDay()
+        {
+            //var rnd = new Random();
+            //var advice = Advices[rnd.Next(Advices.Length)];
+            return Advices[DateTime.Today.DayOfYear % Advices.Length];
+        }
+
+
+    }
+}

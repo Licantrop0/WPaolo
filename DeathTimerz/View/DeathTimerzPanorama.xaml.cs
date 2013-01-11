eae8cde95195c2c68eb7f157c6ef2cad744d4f2b
Licantrop0_cp <SND\Licantrop0_cp>
2013-01-11 10:28:28 +0000

    implementing pin to start

--- DeathTimerz/View/DeathTimerzPanorama.xaml.cs
+++ DeathTimerz/View/DeathTimerzPanorama.xaml.cs
@@ -1,22 +1,19 @@
-﻿using DeathTimerz.Helper;
-using DeathTimerz.Localization;
+﻿using DeathTimerz.Localization;
 using GalaSoft.MvvmLight.Messaging;
 using Microsoft.Advertising.Mobile.UI;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using System;
-using System.IO.IsolatedStorage;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Linq;
 
 namespace DeathTimerz
 {
     public partial class DeathTimerzPanorama : PhoneApplicationPage
     {
         ApplicationBarIconButton EditTestAppBarButton;
+        ApplicationBarIconButton PinToStartAppBarButton;
 
         public DeathTimerzPanorama()
         {
@@ -37,8 +34,7 @@ namespace DeathTimerz
             if (AdPlaceHolder.Children.Count == 0)
             {
 #if DEBUG
-                var ad1 = new AdControl("test_client", "Image480_80", true)
-                { Height = 80, Width = 480 };
+                var ad1 = new AdControl("test_client", "Image480_80", true) { Height = 80, Width = 480 };
 #else
                 var ad1 = new AdControl("d4a3587c-e7e3-4663-972a-dd3c4dd7a3a2",                    
                     "10022419", true) { Height = 80, Width = 480 };
@@ -52,7 +48,7 @@ namespace DeathTimerz
         }
 
         void ad1_ErrorOccurred(object sender, Microsoft.Advertising.AdErrorEventArgs e)
-        {
+        {//Reset height in case the ad is not loaded
             TimeToDeathGrid.Margin = new Thickness(0, -30, 0, 0);
             SuggestionScrollViewer.Margin = new Thickness(0, -30, 0, 0);
         }
@@ -65,18 +61,22 @@ namespace DeathTimerz
 
         private void MainPanorama_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (MainPanorama.SelectedIndex == 2) //Death-Test
+            ApplicationBar.Mode = ApplicationBarMode.Minimized;
+            ApplicationBar.Buttons.Remove(EditTestAppBarButton);
+            ApplicationBar.Buttons.Remove(PinToStartAppBarButton);
+            AdPlaceHolder.Margin = new Thickness(0, 0, 0, 30);
+
+            if (MainPanorama.SelectedIndex == 0) //Health Suggestion
+            {
+                ApplicationBar.Buttons.Add(PinToStartAppBarButton);
+                ApplicationBar.Mode = ApplicationBarMode.Default;
+                AdPlaceHolder.Margin = new Thickness(0, 0, 0, 72);
+            }
+            else if (MainPanorama.SelectedIndex == 2) //Death-Test
             {
                 ApplicationBar.Buttons.Add(EditTestAppBarButton);
                 ApplicationBar.Mode = ApplicationBarMode.Default;
-                //AdPlaceHolder.Height = 122; //80 + (72px - 30) dell'ApplicationBar
                 AdPlaceHolder.Margin = new Thickness(0, 0, 0, 72);
-            }
-            else
-            {
-                ApplicationBar.Mode = ApplicationBarMode.Minimized;
-                ApplicationBar.Buttons.Remove(EditTestAppBarButton);
-                AdPlaceHolder.Margin = new Thickness(0, 0, 0, 30);
             }
         }
 
@@ -86,9 +86,13 @@ namespace DeathTimerz
             EditTestAppBarButton.IconUri = new Uri("Toolkit.Content\\appbar_edit.png", UriKind.Relative);
             EditTestAppBarButton.IsEnabled = AppContext.TimeToDeath.HasValue;
             EditTestAppBarButton.Text = AppResources.EditTest;
-            EditTestAppBarButton.Click += (sender, e) =>
-            { NavigationService.Navigate(new Uri("/View/TestPage.xaml", UriKind.Relative)); };
+            EditTestAppBarButton.Click += (sender, e) => NavigationService.Navigate(new Uri("/View/TestPage.xaml", UriKind.Relative));
 
+
+            PinToStartAppBarButton = new ApplicationBarIconButton();
+            PinToStartAppBarButton.IconUri = new Uri("Toolkit.Content\\appbar_pin.png", UriKind.Relative);
+            PinToStartAppBarButton.Text = AppResources.PinToStart;
+            PinToStartAppBarButton.Click += (sender, e) => CreateSecondaryTile();
 
             var SettingsAppBarMenuItem = new ApplicationBarMenuItem();
             SettingsAppBarMenuItem.Text = Sounds.SoundManager.Instance.MusicEnabled ?
@@ -112,6 +116,17 @@ namespace DeathTimerz
             DisclaimerAppBarMenuItem.Click += (sender, e) =>
             { NavigationService.Navigate(new Uri("/View/DisclaimerPage.xaml", UriKind.Relative)); };
             ApplicationBar.MenuItems.Add(DisclaimerAppBarMenuItem);
+        }
+
+        private void CreateSecondaryTile()
+        {
+
+
+        }
+
+        private void ApplicationBarIconButton_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 

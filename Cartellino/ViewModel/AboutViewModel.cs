aec86c296406f2638ce77b47d3d4534fbc48cae2
Licantrop0_cp <SND\Licantrop0_cp>
2013-01-28 21:30:05 +0000

    upgraded project

--- Cartellino/ViewModel/AboutViewModel.cs
+++ Cartellino/ViewModel/AboutViewModel.cs
@@ -39,7 +39,7 @@ namespace Cartellino.ViewModel
         {
             var wc = new WebClient();
             wc.OpenReadAsync(new Uri(string.Format(
-                 "http://catalog.zune.net/v3.2/{0}/apps?q=WPME&clientType=WinMobile%207.1&store=zest",
+                 "http://marketplaceedgeservice.windowsphone.com/v3.2/{0}/apps?q=WPME&clientType=WinMobile+7.1&store=zest",
                  cultureName)));
 
             wc.OpenReadCompleted += (sender, e) =>
@@ -54,7 +54,7 @@ namespace Cartellino.ViewModel
                           let appId = n.Element(nsAtom + "id").Value.Substring(9)
                           where appId != AppId
                           select new AppTile(new Guid(appId), n.Element(nsAtom + "title").Value, new Uri(
-                              string.Format("http://image.catalog.zune.net/v3.2/{0}/image/{1}?width=200&height=200",
+                              string.Format("http://cdn.marketplaceimages.windowsphone.com/v3.2/{0}/image/{1}?width=200&height=200&resize=true&contenttype=image/png",
                                   cultureName, imageId)));
             };
         }

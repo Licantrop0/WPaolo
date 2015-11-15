d22f27e879be9ab9475825314407a5a7b170129e
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2015-11-15 01:49:20 +0000

    now it works!

---
+++ EasyCall/ViewModel/NumberViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Input;
+using EasyCall.Helper;
+using WPCommon.Helpers;
+
+namespace EasyCall.ViewModel
+{
+    public class NumberViewModel
+    {
+        public string Number { get; }
+        public string Name { get; }
+
+        public NumberViewModel(string number, string name)
+        {
+            Number = Regex.Replace(number, @"[\s\-\(\)]", string.Empty);
+            Name = name;
+        }
+
+        private ICommand _callNumberCommand;
+        public ICommand CallNumberCommand => _callNumberCommand ??
+            (_callNumberCommand = new RelayCommand(
+                o => CallHelper.Call(Name, Number)));
+
+        private ICommand _sendSmsCommand;
+        public ICommand SendSmsCommand => _sendSmsCommand ??
+            (_sendSmsCommand = new RelayCommand(
+                o => CallHelper.SendSms(Number)));
+    }
+}

3f58fa39e84d3e4836b38badb407a8fd90927684
Luca Spolidoro <Licantrop0@users.noreply.github.com>
2016-01-06 21:30:30 +0000

    VolumeDetectionEffect

--- TouchColors/TouchColors/Controls/MicrophoneVisualizer.xaml.cs
+++ TouchColors/TouchColors/Controls/MicrophoneVisualizer.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using Windows.Foundation.Collections;
 using Windows.Media.Audio;
 using Windows.Media.Capture;
+using Windows.Media.Effects;
 using Windows.Media.Render;
 using Windows.UI.Xaml.Controls;
 
@@ -8,6 +10,9 @@ namespace TouchColors.Controls
 {
     public sealed partial class MicrophoneVisualizer : UserControl
     {
+        private int _scoreInterval = 0;
+        private IPropertySet _volumeDetectionConfiguration;
+
         public MicrophoneVisualizer()
         {
             this.InitializeComponent();
@@ -23,8 +28,39 @@ namespace TouchColors.Controls
             var deviceInputNodeResult = await audioGraph.CreateDeviceInputNodeAsync(MediaCategory.Speech);
             var deviceInput = deviceInputNodeResult.DeviceInputNode;
 
-            //https://channel9.msdn.com/events/Build/2015/3-634 balasiv@microsoft.com
-            //deviceInput.EffectDefinitions.Add(new AudioEffectDefinition(typeof(VolumeDetectionEffect)
+            _volumeDetectionConfiguration = new PropertySet { { "Volume", 0d } };
+            deviceInput.EffectDefinitions.Add(
+                new AudioEffectDefinition(typeof(VolumeDetectionEffect).FullName,
+                _volumeDetectionConfiguration));
+            audioGraph.QuantumProcessed += Graph_QuantumProcessed;
+        }
+
+        private async void Graph_QuantumProcessed(AudioGraph sender, object args)
+        {
+            // SCORE: Find the current volume level every 20 intervals
+            if (_scoreInterval <= 20)
+            {
+                _scoreInterval++;
+                return;
+            }
+
+            var temp = double.Parse(_volumeDetectionConfiguration["Volume"].ToString());
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                if (temp >= -140 && temp < -60)
+                    this.Score.Text = ""; // no bars
+                else if (temp >= -60 && temp < -30)
+                    this.Score.Text = ""; // one bar
+                else if (temp >= -30 && temp < -15)
+                    this.Score.Text = ""; // two bar
+                else if (temp >= -15 && temp < -7)
+                    this.Score.Text = ""; // three bars
+                else if (temp >= -7)
+                    this.Score.Text = ""; // four/full bars
+            });
+
+            // Reset score interval
+            _scoreInterval = 0;
         }
     }
 }
